8ee68fed263bdd298a7f08efaa8da7578cad8935
Stephan Prantl <stephan.prantl@gmail.com>
2020-10-26 11:14:38 +0100

    build

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -8,9 +8,9 @@ repositories {
 plugins {
     id("org.springframework.boot") version "2.3.4.RELEASE"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
-    kotlin("jvm") version "1.4.0"
-    kotlin("plugin.spring") version "1.4.0"
-    kotlin("plugin.jpa") version "1.4.0"
+    kotlin("jvm") version "1.4.10"
+    kotlin("plugin.spring") version "1.4.10"
+    kotlin("plugin.jpa") version "1.4.10"
 }
 
 group = "at.tafel1030.admin"
---
+++ frontend/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("com.github.node-gradle.node") version "2.2.4"
+}
+
+node {
+    download = true
+    version = "12.19.0"
+}
