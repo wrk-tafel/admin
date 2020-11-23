849c351d3c1dd2d5abcefb28050773afb07184b4
Stephan Prantl <stephan.prantl@gmail.com>
2020-11-23 23:28:43 +0100

    activated sourcemap

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,6 +2,7 @@ module.exports = {
   lintOnSave: false,
   runtimeCompiler: true,
   configureWebpack: {
+    devtool: 'source-map',
     //Necessary to run npm link https://webpack.js.org/configuration/resolve/#resolve-symlinks
     resolve: {
        symlinks: false
