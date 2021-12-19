6e1beb14ae1ea8b992c0b4d56c674d7146518be9
Stephan Prantl <stephan.prantl@gmail.com>
2021-12-19 23:03:00 +0100

    testing authorization stuff

--- backend/src/main/kotlin/at/wrk/tafel/admin/backend/security/components/JwtUserDetailsService.kt
+++ backend/src/main/kotlin/at/wrk/tafel/admin/backend/security/components/JwtUserDetailsService.kt
@@ -1,5 +1,6 @@
 package at.wrk.tafel.admin.backend.security.components
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
@@ -16,6 +17,12 @@ class JwtUserDetailsService : UserDetailsService {
             User(
                 "javainuse", "$2a$10\$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
                 listOf()
+            )
+        )
+        inMemoryUserDetailsManager.createUser(
+            User(
+                "checkin", "$2a$10\$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+                listOf(SimpleGrantedAuthority("CHECKIN"))
             )
         )
     }
--- backend/src/main/kotlin/at/wrk/tafel/admin/backend/test/TestController.kt
+++ backend/src/main/kotlin/at/wrk/tafel/admin/backend/test/TestController.kt
@@ -1,14 +1,24 @@
 package at.wrk.tafel.admin.backend.test
 
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/test")
 class TestController {
 
-    @GetMapping("test")
-    fun test(): String {
-        return "testResponse"
+    @GetMapping("/authenticated")
+    @PreAuthorize("isAuthenticated()")
+    fun authenticated(): String {
+        return "authenticated response"
+    }
+
+    @GetMapping("/checkin")
+    @PreAuthorize("hasAuthority('CHECKIN')")
+    fun checkin(): String {
+        return "checkin response"
     }
 
 }
