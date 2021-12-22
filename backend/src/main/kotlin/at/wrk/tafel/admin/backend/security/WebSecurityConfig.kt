a259cfd83a7d924a14a3e5f01b254902c6a1ba1c
Stephan Prantl <stephan.prantl@gmail.com>
2021-12-22 22:20:54 +0100

    replaced with default entrypoint

--- backend/src/main/kotlin/at/wrk/tafel/admin/backend/security/WebSecurityConfig.kt
+++ backend/src/main/kotlin/at/wrk/tafel/admin/backend/security/WebSecurityConfig.kt
@@ -2,9 +2,9 @@ package at.wrk.tafel.admin.backend.security
 
 import at.wrk.tafel.admin.backend.security.components.JwtCheckTokenFilter
 import at.wrk.tafel.admin.backend.security.components.JwtUserDetailsService
-import at.wrk.tafel.admin.backend.security.components.RestAuthenticationEntryPoint
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
@@ -14,13 +14,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.authentication.HttpStatusEntryPoint
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 class WebSecurityConfig(
-    private val restAuthenticationEntryPoint: RestAuthenticationEntryPoint,
     private val userDetailsService: JwtUserDetailsService,
     private val jwtCheckTokenFilter: JwtCheckTokenFilter
 ) : WebSecurityConfigurerAdapter() {
@@ -48,7 +48,7 @@ class WebSecurityConfig(
             .authenticated()
             .and()
             .exceptionHandling() // make sure we use stateless session; session won't be used to store user's state.
-            .authenticationEntryPoint(restAuthenticationEntryPoint)
+            .authenticationEntryPoint(HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
             .and().sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
