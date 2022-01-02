da51524554f12f99da161c82fe033dc89ae47fdd
Stephan Prantl <stephan.prantl@gmail.com>
2022-01-02 23:15:30 +0100

    fixed archunit

--- backend/src/test/kotlin/at/wrk/tafel/admin/backend/architecture/GeneralCodingRulesTest.kt
+++ backend/src/test/kotlin/at/wrk/tafel/admin/backend/architecture/GeneralCodingRulesTest.kt
@@ -2,7 +2,9 @@ package at.wrk.tafel.admin.backend.architecture
 
 import com.tngtech.archunit.junit.AnalyzeClasses
 import com.tngtech.archunit.junit.ArchTest
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noFields
 import com.tngtech.archunit.library.GeneralCodingRules
+import com.tngtech.archunit.library.GeneralCodingRules.BE_ANNOTATED_WITH_AN_INJECTION_ANNOTATION
 
 @AnalyzeClasses(packages = ["at.wrk.tafel.admin.backend"])
 class GeneralCodingRulesTest {
@@ -11,7 +13,10 @@ class GeneralCodingRulesTest {
     val `stdout and stderr shouldn't be used` = GeneralCodingRules.NO_CLASSES_SHOULD_ACCESS_STANDARD_STREAMS
 
     @ArchTest
-    val `no classes should use field injection` = GeneralCodingRules.NO_CLASSES_SHOULD_USE_FIELD_INJECTION
+    val `no classes should use field injection` =
+        noFields().that().areDeclaredInClassesThat().haveSimpleNameNotEndingWith("Test")
+            .and().areDeclaredInClassesThat().haveSimpleNameNotEndingWith("IT")
+            .should(BE_ANNOTATED_WITH_AN_INJECTION_ANNOTATION)
 
     @ArchTest
     val `generic exceptions shouldn't be thrown` = GeneralCodingRules.NO_CLASSES_SHOULD_THROW_GENERIC_EXCEPTIONS
