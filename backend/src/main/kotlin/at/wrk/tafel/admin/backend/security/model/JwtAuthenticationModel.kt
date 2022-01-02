2f318db24e860c6eb4e71eee86896a6102bb9db4
Stephan Prantl <stephan.prantl@gmail.com>
2022-01-02 03:30:17 +0100

    added test

--- backend/src/main/kotlin/at/wrk/tafel/admin/backend/security/model/JwtAuthenticationModel.kt
+++ backend/src/main/kotlin/at/wrk/tafel/admin/backend/security/model/JwtAuthenticationModel.kt
@@ -3,7 +3,7 @@ package at.wrk.tafel.admin.backend.security.model
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 
 data class JwtAuthenticationToken(
-    val tokenString: String
+    val tokenValue: String
 ) : UsernamePasswordAuthenticationToken(null, null)
 
 data class JwtResponse(
