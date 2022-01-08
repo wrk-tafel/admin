7591d21c5e4528ac925ecd6fb3237283bfe9cb80
Stephan Prantl <stephan.prantl@gmail.com>
2022-01-09 00:00:24 +0100

    cleanup

--- frontend/src/main/webapp/src/app/common/security/authguard.service.ts
+++ frontend/src/main/webapp/src/app/common/security/authguard.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivateChild, Router, RouterStateSnapshot } from '@angular/router';
 import { AuthenticationService } from './authentication.service';
 
 @Injectable({
