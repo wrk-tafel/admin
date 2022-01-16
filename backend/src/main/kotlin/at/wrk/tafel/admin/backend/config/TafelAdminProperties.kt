af2e6f7c29680e99acc52cd50787070c5bb7c528
Stephan Prantl <stephan.prantl@gmail.com>
2022-01-16 21:02:48 +0100

    baseurl config

---
+++ backend/src/main/kotlin/at/wrk/tafel/admin/backend/config/TafelAdminProperties.kt
@@ -0,0 +1,10 @@
+package at.wrk.tafel.admin.backend.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+
+@ConfigurationProperties(prefix = "tafeladmin")
+@ConstructorBinding
+data class TafelAdminProperties(
+    val baseUrl: String
+)
