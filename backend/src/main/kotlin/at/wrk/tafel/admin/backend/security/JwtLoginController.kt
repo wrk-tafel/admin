89f177b952009064f22f8993106e43538288cf46
Stephan Prantl <stephan.prantl@gmail.com>
2022-02-05 20:37:49 +0100

    fixed log content

--- backend/src/main/kotlin/at/wrk/tafel/admin/backend/security/JwtLoginController.kt
+++ backend/src/main/kotlin/at/wrk/tafel/admin/backend/security/JwtLoginController.kt
@@ -26,7 +26,7 @@ class JwtLoginController(
             val token: String? = user.username.let { jwtTokenService.generateToken(it, user.authorities) }
 
             token.let {
-                logger.info("Login successful - User '${user.username}' from IP: ${getIpAddress(request)}")
+                logger.info("Login successful via user '${user.username}' from IP: ${getIpAddress(request)}")
 
                 return ResponseEntity.ok(JwtResponse(token!!))
             }
