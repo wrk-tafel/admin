1c7d7f5315765d82507009dcc9b759acceee8988
Stephan Prantl <stephan.prantl@gmail.com>
2022-02-06 21:55:54 +0100

    added authinterceptor tests

--- frontend/src/main/webapp/src/app/common/http/authentication-interceptor.service.spec.ts
+++ frontend/src/main/webapp/src/app/common/http/authentication-interceptor.service.spec.ts
@@ -1,18 +1,56 @@
+import { HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { AuthenticationService } from '../security/authentication.service';
 
 import { AuthenticationInterceptor } from './authentication-interceptor.service';
 
-describe('JwtTokenInterceptor', () => {
-  let service: AuthenticationInterceptor;
+describe('AuthenticationInterceptor', () => {
+  let client: HttpClient
+  let httpMock: HttpTestingController
+  let authServiceSpy: jasmine.SpyObj<AuthenticationService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthenticationInterceptor);
+    const spy = jasmine.createSpyObj('AuthenticationService', ['isAuthenticated', 'getToken']);
+
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        AuthenticationInterceptor,
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: AuthenticationInterceptor,
+          multi: true
+        },
+        {
+          provide: AuthenticationService,
+          useValue: spy
+        }
+      ],
+    });
+
+    client = TestBed.inject(HttpClient)
+    httpMock = TestBed.inject(HttpTestingController);
+    authServiceSpy = TestBed.inject(AuthenticationService) as jasmine.SpyObj<AuthenticationService>;
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('not authenticated - called without authorization header', () => {
+    authServiceSpy.isAuthenticated.and.returnValue(false)
+
+    client.get('/test').subscribe()
+
+    let req = httpMock.expectOne('/test')
+    expect(req.request.headers.get('Authorization')).toBeNull
   });
+
+  it('authenticated - called with authorization header', () => {
+    authServiceSpy.isAuthenticated.and.returnValue(true)
+    authServiceSpy.getToken.and.returnValue("TOKENVALUE")
+
+    client.get('/test').subscribe()
+
+    let req = httpMock.expectOne('/test')
+    expect(req.request.headers.get('Authorization')).toEqual('Bearer TOKENVALUE')
+  });
+
 });
-
-// TODO
