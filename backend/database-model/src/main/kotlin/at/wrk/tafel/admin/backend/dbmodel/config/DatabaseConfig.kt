6e3544f3a4aa60767b2758ed69f485c0dfd83d54
Stephan Prantl <stephan.prantl@gmail.com>
2022-02-19 16:53:25 +0100

    refined

--- backend/database-model/src/main/kotlin/at/wrk/tafel/admin/backend/dbmodel/config/DatabaseConfig.kt
+++ backend/database-model/src/main/kotlin/at/wrk/tafel/admin/backend/dbmodel/config/DatabaseConfig.kt
@@ -1,8 +1,10 @@
 package at.wrk.tafel.admin.backend.dbmodel.config
 
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @Configuration
-@EnableJpaRepositories("at.wrk.tafel.admin.backend")
+@EnableJpaRepositories("at.wrk.tafel.admin.backend.repositories")
+@EntityScan("at.wrk.tafel.admin.backend.entities")
 class DatabaseConfig
