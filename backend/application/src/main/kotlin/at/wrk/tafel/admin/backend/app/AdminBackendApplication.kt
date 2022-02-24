fb7a3c7337348e6263e29c27fe2cd642f65f6c6e
Stephan Prantl <stephan.prantl@gmail.com>
2022-02-24 22:17:24 +0100

    removed enablewebmvc to re-active root forwarding

--- backend/application/src/main/kotlin/at/wrk/tafel/admin/backend/app/AdminBackendApplication.kt
+++ backend/application/src/main/kotlin/at/wrk/tafel/admin/backend/app/AdminBackendApplication.kt
@@ -4,11 +4,9 @@ import at.wrk.tafel.admin.backend.common.ExcludeFromTestCoverage
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 @ConfigurationPropertiesScan("at.wrk.tafel.admin.backend")
 @ExcludeFromTestCoverage
-@EnableWebMvc
 @SpringBootApplication(scanBasePackages = ["at.wrk.tafel.admin.backend"])
 class AdminBackendApplication
 
