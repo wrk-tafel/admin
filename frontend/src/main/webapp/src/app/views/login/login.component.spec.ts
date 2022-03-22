c3beba9157f583f87497285d4f5a9fb7159c5470
Stephan Prantl <stephan.prantl@gmail.com>
2022-03-22 21:18:36 +0100

    first tests for login component

---
+++ frontend/src/main/webapp/src/app/views/login/login.component.spec.ts
@@ -0,0 +1,64 @@
+import { fakeAsync, TestBed, waitForAsync } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthenticationService } from '../../common/security/authentication.service';
+import { LoginComponent } from './login.component';
+
+describe('LoginComponent', () => {
+  let authService: jasmine.SpyObj<AuthenticationService>;
+  let router: jasmine.SpyObj<Router>;
+
+  beforeEach(waitForAsync(() => {
+    const authServiceSpy = jasmine.createSpyObj('AuthenticationService', ['login', 'removeToken']);
+    const routerSpy = jasmine.createSpyObj('Router', ['navigate', 'getCurrentNavigation']);
+
+    TestBed.configureTestingModule({
+      declarations: [
+        LoginComponent
+      ],
+      providers: [
+        {
+          provide: AuthenticationService,
+          useValue: authServiceSpy
+        },
+        {
+          provide: Router,
+          useValue: routerSpy
+        }
+      ],
+      imports: [RouterTestingModule, FormsModule]
+    }).compileComponents();
+
+    authService = TestBed.inject(AuthenticationService) as jasmine.SpyObj<AuthenticationService>;
+    router = TestBed.inject(Router) as jasmine.SpyObj<Router>;
+  }));
+
+  it('should create the app', waitForAsync(() => {
+    const fixture = TestBed.createComponent(LoginComponent);
+    const app = fixture.debugElement.componentInstance;
+
+    expect(app).toBeTruthy();
+  }));
+
+  it('onClickSubmit - login successful', waitForAsync(() => {
+    const fixture = TestBed.createComponent(LoginComponent);
+    const component = fixture.componentInstance;
+    authService.login.and.returnValue(Promise.resolve(true));
+
+    component.onClickSubmit({ username: 'test', password: 'pwd' }).then(() => {
+      expect(router.navigate).toHaveBeenCalledWith(['uebersicht']);
+    });
+  }));
+
+  it('onClickSubmit - login failed', waitForAsync(() => {
+    const fixture = TestBed.createComponent(LoginComponent);
+    const component = fixture.componentInstance;
+    authService.login.and.returnValue(Promise.resolve(false));
+
+    component.onClickSubmit({ username: 'test', password: 'pwd' }).then(() => {
+      expect(component.errorMsg).toBe('Anmeldung fehlgeschlagen!');
+    });
+  }));
+
+});
