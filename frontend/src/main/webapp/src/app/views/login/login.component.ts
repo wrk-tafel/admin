c64e13b040e314d214350a8c28c718cf93009970
Stephan Prantl <stephan.prantl@gmail.com>
2022-03-26 02:41:01 +0100

    removed unused imports

--- frontend/src/main/webapp/src/app/views/login/login.component.ts
+++ frontend/src/main/webapp/src/app/views/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../../common/security/authentication.service';
