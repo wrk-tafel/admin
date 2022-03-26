04f8f737fe36dac3670f80d97d8b2a3fd69cdce7
Stephan Prantl <stephan.prantl@gmail.com>
2022-03-27 00:15:00 +0100

    defined reactive form

--- frontend/src/main/webapp/src/app/views/customer/components/customer-form.component.ts
+++ frontend/src/main/webapp/src/app/views/customer/components/customer-form.component.ts
@@ -1,29 +1,29 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'customer-form',
   templateUrl: 'customer-form.component.html'
 })
 export class CustomerFormComponent {
-  customer: Customer = {};
+  customerForm = new FormGroup({
+    lastname: new FormControl('', Validators.required),
+    firstname: new FormControl('', Validators.required),
+    nationality: new FormControl('', Validators.required),
+    street: new FormControl('', Validators.required),
+    houseNumber: new FormControl('', Validators.required),
+    stair: new FormControl(''),
+    door: new FormControl('', Validators.required),
+    postalCode: new FormControl('1030', Validators.required),
+    city: new FormControl('Wien', Validators.required),
+    birthDate: new FormControl('', Validators.required),
+    employer: new FormControl('', Validators.required),
+    income: new FormControl('', Validators.required),
+    incomeDue: new FormControl('', Validators.required)
+  });
 
-  public showLastName() {
-    console.log("DEBUG", this.customer)
+  public save() {
+    // TODO
+    console.log("SAVE CALLED");
   }
-}
-
-interface Customer {
-  lastName?: String
-  firstName?: String
-  nationality?: String
-  street?: String
-  houseNumber?: String
-  stair?: String
-  door?: String
-  postalCode?: number
-  city?: String
-  birthDate?: Date
-  employer?: String
-  income?: number
-  incomeDue?: Date
 }
--- frontend/src/main/webapp/src/app/views/customer/customer.module.ts
+++ frontend/src/main/webapp/src/app/views/customer/customer.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { CustomerComponent } from './customer.component';
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerFormComponent } from './components/customer-form.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   imports: [
-    FormsModule,
+    CommonModule,
+    ReactiveFormsModule,
     CustomerRoutingModule
   ],
   declarations: [CustomerComponent, CustomerFormComponent]
