7376e1d9e2b8938886cc57c7cb4667c6e16cc9ae
Stephan Prantl <stephan.prantl@gmail.com>
2022-04-10 20:28:19 +0200

    fixed api call

--- frontend/src/main/webapp/src/app/views/customer/api/customer-api.service.ts
+++ frontend/src/main/webapp/src/app/views/customer/api/customer-api.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class CustomerApiService {
     private http: HttpClient
   ) { }
 
-  validate(data: CustomerRequestData): ValidateCustomerResponse {
+  validate(data: CustomerRequestData): Observable<ValidateCustomerResponse> {
     return this.http.post<ValidateCustomerResponse>('/customers/validate', data);
   }
 
