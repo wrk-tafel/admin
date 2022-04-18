f74fcedac30528f25b7ecd09f4aa167b2a13f865
Stephan Prantl <stephan.prantl@gmail.com>
2022-04-18 12:53:30 +0200

    different structure with formatting

--- frontend/src/main/webapp/src/app/views/customer/views/customer-detail.component.ts
+++ frontend/src/main/webapp/src/app/views/customer/views/customer-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import * as moment from 'moment';
 import { CustomerApiService, CustomerData } from '../api/customer-api.service';
 
 @Component({
@@ -7,7 +8,7 @@ import { CustomerApiService, CustomerData } from '../api/customer-api.service';
   templateUrl: 'customer-detail.component.html'
 })
 export class CustomerDetailComponent implements OnInit {
-  customerData: CustomerData;
+  customerDetailData: CustomerDetailData;
 
   constructor(
     private route: ActivatedRoute,
@@ -16,8 +17,50 @@ export class CustomerDetailComponent implements OnInit {
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.apiService.getCustomer(+params['id']).subscribe((customerData) => {
-        this.customerData = customerData;
+        this.customerDetailData = this.mapCustomerDataForView(customerData);
       });
     });
   }
+
+  private mapCustomerDataForView(customerData: CustomerData): CustomerDetailData {
+    return {
+      id: customerData.id,
+      customerId: customerData.customerId,
+      firstname: customerData.firstname,
+      lastname: customerData.lastname,
+      birthDate: moment(customerData.birthDate).format('DD.MM.YYYY'),
+      country: customerData.country, // TODO map to name
+      telephoneNumber: customerData.telephoneNumber,
+      email: customerData.email,
+      addressStreet: customerData.address.street,
+      addressHouseNumber: customerData.address.houseNumber,
+      addressStairway: customerData.address.stairway,
+      addressDoor: customerData.address.door,
+      addressPostalCode: customerData.address.postalCode,
+      addressCity: customerData.address.city,
+      employer: customerData.employer,
+      income: customerData.income,
+      incomeDue: moment(customerData.incomeDue).format('DD.MM.YYYY')
+    }
+  }
+}
+
+interface CustomerDetailData {
+  id?: number;
+  customerId?: number;
+  firstname: string;
+  lastname: string;
+  birthDate: string;
+  country: string;
+  telephoneNumber: number;
+  email: string;
+  addressStreet: string;
+  addressHouseNumber: string;
+  addressStairway: string;
+  addressDoor: string;
+  addressPostalCode: number;
+  addressCity: string;
+  employer: string;
+  income: number;
+  incomeDue: string;
 }
