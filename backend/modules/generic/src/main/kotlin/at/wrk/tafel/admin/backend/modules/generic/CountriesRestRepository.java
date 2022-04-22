1a7ac7179343dec81730418ced0498395b062cc9
Stephan Prantl <stephan.prantl@gmail.com>
2022-04-22 18:55:17 +0200

    deleted old code

--- backend/modules/generic/src/main/kotlin/at/wrk/tafel/admin/backend/modules/generic/CountriesRestRepository.java
+++
@@ -1,9 +0,0 @@
-package at.wrk.tafel.admin.backend.modules.base;
-
-import at.wrk.tafel.admin.backend.dbmodel.entities.staticdata.CountryEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(path = "countries", collectionResourceRel = "countries")
-public interface CountriesRestRepository extends CrudRepository<CountryEntity, Long> {
-}
