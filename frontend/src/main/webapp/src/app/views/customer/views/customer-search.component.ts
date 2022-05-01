4c76d2fc7e59fd4c7de1a76d2c62621bcc4e2d98
Stephan Prantl <stephan.prantl@gmail.com>
2022-05-02 00:48:31 +0200

    added comment

--- frontend/src/main/webapp/src/app/views/customer/views/customer-search.component.ts
+++ frontend/src/main/webapp/src/app/views/customer/views/customer-search.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { CustomerApiService } from '../api/customer-api.service';
 
 @Component({
   selector: 'customer-search',
   templateUrl: 'customer-search.component.html'
 })
 export class CustomerSearchComponent {
+  constructor(
+    private customerApiService: CustomerApiService
+  ) { }
 
   customerSearchForm = new FormGroup({
     customerId: new FormControl(''),
@@ -14,8 +18,20 @@ export class CustomerSearchComponent {
   });
 
   search() {
-    console.log("SEARCH", this.customerSearchForm);
+    if (this.customerId) {
+      // TODO router / navigate, maybe? Check if exists? on detailpage?
+    }
+    else {
+      this.customerApiService.searchCustomer(this.customerSearchForm.value).subscribe((res) => {
+        // TODO
+        console.log("RESPONSE", res);
+      });
+    }
   }
+
+  get customerId() { return this.customerSearchForm.get('customerId'); }
+  get lastname() { return this.customerSearchForm.get('lastname'); }
+  get firstname() { return this.customerSearchForm.get('firstname'); }
 }
 
 export interface CustomerSearchFormData {
