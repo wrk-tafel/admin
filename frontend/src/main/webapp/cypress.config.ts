53123246f3e6e3a34c8de0727cb13b4c98b8b212
Stephan Prantl <stephan.prantl@gmail.com>
2022-07-02 01:46:42 +0200

    migrated cypress to version 10

---
+++ frontend/src/main/webapp/cypress.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  builder: '@cypress/schematic:cypress',
+  viewportWidth: 1280,
+  viewportHeight: 1024,
+  videoCompression: false,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'http://localhost:4200/',
+    experimentalSessionAndOrigin: true,
+  },
+})
