ddd42f3d8d95e17839a2356ec6d2353e4285ef6f
Stephan Prantl <stephan.prantl@gmail.com>
2022-10-30 17:49:40 +0100

    added e2e test and updated package-lock

---
+++ frontend/src/main/webapp/cypress/e2e/passwordchange.cy.ts
@@ -0,0 +1,34 @@
+describe('PasswordChange', () => {
+
+  beforeEach(() => {
+    cy.login('e2etest2', 'e2etest');
+    cy.visit('/#');
+  });
+
+  it('change password', () => {
+    cy.byTestId('usermenu').click();
+    cy.byTestId('usermenu-changepassword').click();
+
+    cy.byTestId('currentPasswordText').type('e2etest');
+    cy.byTestId('newPasswordText').type('4wtouCcWWqDJsP');
+    cy.byTestId('newRepeatedPasswordText').type('4wtouCcWWqDJsP');
+
+    cy.byTestId('saveButton').click();
+
+    cy.byTestId('usermenu').click();
+    cy.byTestId('usermenu-logout').click();
+
+    cy.url().should('contain', '/login');
+
+    cy.login('e2etest2', '4wtouCcWWqDJsP');
+    cy.visit('/#');
+
+    cy.url().should('contain', '/#');
+
+    // expect error for old password
+    cy.createLoginRequest('e2etest2', 'e2etest', false).then((resp) => {
+      expect(resp.status).to.eq(403)
+    });
+  });
+
+});
