098cc5171ee90c15648d45311e13572640a22102
Stephan Prantl <stephan.prantl@gmail.com>
2022-10-31 00:06:20 +0100

    improvement try

--- frontend/src/main/webapp/cypress/e2e/passwordchange.cy.ts
+++ frontend/src/main/webapp/cypress/e2e/passwordchange.cy.ts
@@ -1,3 +1,5 @@
+import {recurse} from "cypress-recurse";
+
 describe('PasswordChange', () => {
 
   beforeEach(() => {
@@ -9,10 +11,23 @@ describe('PasswordChange', () => {
     cy.byTestId('usermenu').click();
     cy.byTestId('usermenu-changepassword').click();
 
-    cy.byTestId('currentPasswordText').type('e2etest')
-      .then(() => cy.byTestId('newPasswordText').type('4wtouCcWWqDJsP'))
-      .then(() => cy.byTestId('newRepeatedPasswordText').type('4wtouCcWWqDJsP'))
-      .then(() => cy.byTestId('saveButton').click());
+    cy.byTestId('currentPasswordText').type('e2etest');
+
+    const newPassword = '4wtouCcWWqDJsP';
+    recurse(
+      // the commands to repeat, and they yield the input element
+      () => cy.byTestId('newPasswordText').type(newPassword),
+      // the predicate takes the output of the above commands
+      // and returns a boolean. If it returns true, the recursion stops
+      ($input) => $input.val() === newPassword,
+    )
+      // the recursion yields whatever the command function yields
+      // and we can confirm that the text was entered correctly
+      .should('have.value', newPassword);
+
+    cy.byTestId('newRepeatedPasswordText').type('4wtouCcWWqDJsP');
+
+    cy.byTestId('saveButton').click();
 
     cy.byTestId('usermenu').click();
     cy.byTestId('usermenu-logout').click();
