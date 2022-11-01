906f7f5e1f0b2318f40a4317c975bc3279116eb2
Stephan Prantl <stephan.prantl@gmail.com>
2022-11-01 23:12:24 +0100

    added buttons

--- frontend/src/main/webapp/src/app/views/common/login-passwordchange/login-passwordchange.component.ts
+++ frontend/src/main/webapp/src/app/views/common/login-passwordchange/login-passwordchange.component.ts
@@ -1,9 +1,33 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {PasswordChangeFormComponent} from "../../user/views/passwordchange-form/passwordchange-form.component";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'tafel-login-passwordchange',
   templateUrl: 'login-passwordchange.component.html'
 })
 export class LoginPasswordChangeComponent {
+  constructor(
+    private router: Router
+  ) {
+  }
+
+  @ViewChild(PasswordChangeFormComponent) public form: PasswordChangeFormComponent;
+
+  changePassword() {
+    this.form.changePassword().subscribe(successful => {
+      if (successful) {
+        // TODO
+      }
+    });
+  }
+
+  cancel() {
+    this.router.navigate(['login']);
+  }
+
+  isSaveDisabled(): Boolean {
+    return !this.form?.form.valid;
+  }
 
 }
