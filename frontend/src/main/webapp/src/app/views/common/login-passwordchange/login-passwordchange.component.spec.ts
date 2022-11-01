60e0def2e3ed376e87f849978ca698a1d8f76519
Stephan Prantl <stephan.prantl@gmail.com>
2022-11-01 22:51:45 +0100

    added empty login passwordchange component

---
+++ frontend/src/main/webapp/src/app/views/common/login-passwordchange/login-passwordchange.component.spec.ts
@@ -0,0 +1,22 @@
+import {TestBed, waitForAsync} from '@angular/core/testing';
+import {ReactiveFormsModule} from '@angular/forms';
+import {LoginPasswordChangeComponent} from "./login-passwordchange.component";
+
+describe('LoginPasswordChangeComponent', () => {
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
+      declarations: [LoginPasswordChangeComponent]
+    }).compileComponents();
+  }));
+
+  it('should create the component', waitForAsync(() => {
+    const fixture = TestBed.createComponent(LoginPasswordChangeComponent);
+    const component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    expect(component).toBeTruthy();
+  }));
+
+});
---
+++ frontend/src/main/webapp/src/app/views/common/login-passwordchange/login-passwordchange.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'tafel-login-passwordchange',
+  templateUrl: 'login-passwordchange.component.html'
+})
+export class LoginPasswordChangeComponent {
+
+}
