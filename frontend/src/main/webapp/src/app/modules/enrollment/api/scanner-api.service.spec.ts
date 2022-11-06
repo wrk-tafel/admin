c2a365717b3473c4fb2e3a45b0893f18e22a84f8
Stephan Prantl <stephan.prantl@gmail.com>
2022-11-06 22:13:54 +0100

    added api service

---
+++ frontend/src/main/webapp/src/app/modules/enrollment/api/scanner-api.service.spec.ts
@@ -0,0 +1,15 @@
+import {TestBed} from '@angular/core/testing';
+import {ScannerApiService} from "./scanner-api.service";
+
+describe('CustomerApiService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ScannerApiService]
+    });
+  });
+
+  it('TODO', () => {
+    expect(false).toBeTrue();
+  });
+
+});
