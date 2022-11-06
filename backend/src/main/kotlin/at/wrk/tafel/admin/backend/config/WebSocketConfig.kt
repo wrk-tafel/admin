fb0db59e3d586f6c3b311f85e4c416884755437a
Stephan Prantl <stephan.prantl@gmail.com>
2022-11-06 23:05:35 +0100

    config improved

--- backend/src/main/kotlin/at/wrk/tafel/admin/backend/config/WebSocketConfig.kt
+++ backend/src/main/kotlin/at/wrk/tafel/admin/backend/config/WebSocketConfig.kt
@@ -11,7 +11,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 class WebSocketConfig : WebSocketMessageBrokerConfigurer {
 
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
-        registry.addEndpoint("/ws-api/scanners")
+        registry.addEndpoint("/ws-api")
     }
 
     override fun configureMessageBroker(registry: MessageBrokerRegistry) {
