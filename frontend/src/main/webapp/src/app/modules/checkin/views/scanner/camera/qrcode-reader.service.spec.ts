2331304f776ae09242a70cee664f9568b08770c5
Stephan Prantl <stephan.prantl@gmail.com>
2022-11-08 23:26:50 +0100

    added test

--- frontend/src/main/webapp/src/app/modules/checkin/views/scanner/camera/qrcode-reader.service.spec.ts
+++ frontend/src/main/webapp/src/app/modules/checkin/views/scanner/camera/qrcode-reader.service.spec.ts
@@ -1,15 +1,25 @@
 import {QRCodeReaderService} from "./qrcode-reader.service";
+import {Html5Qrcode} from "html5-qrcode";
+import {CameraDevice} from "html5-qrcode/esm/core";
+import {of} from "rxjs";
 
 describe('QRCodeReaderService', () => {
+  const testCameras: Array<CameraDevice> = Array({id: '1', label: 'cam1'}, {id: '2', label: 'cam2'});
+
   function setup() {
+    const qrCodeSpy = spyOn(Html5Qrcode, 'getCameras').and.returnValue(of(testCameras).toPromise());
+
     const service = new QRCodeReaderService();
-    return {service};
+    return {service, qrCodeSpy};
   }
 
-  it('TODO', () => {
-    const {service} = setup();
+  it('getCameras returns correct result', async () => {
+    const {service, qrCodeSpy} = setup();
 
-    expect(false).toBeTrue();
+    const cameras = await service.getCameras();
+
+    expect(qrCodeSpy).toHaveBeenCalled();
+    expect(cameras).toEqual(testCameras);
   });
 
 });
