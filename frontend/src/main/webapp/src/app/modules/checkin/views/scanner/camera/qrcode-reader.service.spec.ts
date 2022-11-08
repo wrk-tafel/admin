5224238a14f383fcf346af4f0bde42af8b3b6fdf
Stephan Prantl <stephan.prantl@gmail.com>
2022-11-08 23:31:49 +0100

    added test

--- frontend/src/main/webapp/src/app/modules/checkin/views/scanner/camera/qrcode-reader.service.spec.ts
+++ frontend/src/main/webapp/src/app/modules/checkin/views/scanner/camera/qrcode-reader.service.spec.ts
@@ -4,9 +4,12 @@ import {CameraDevice} from "html5-qrcode/esm/core";
 import {of} from "rxjs";
 
 describe('QRCodeReaderService', () => {
+  const LOCAL_STORAGE_LAST_CAMERA_ID_KEY = 'TAFEL_LAST_CAMERA_ID';
   const testCameras: Array<CameraDevice> = Array({id: '1', label: 'cam1'}, {id: '2', label: 'cam2'});
 
   function setup() {
+    localStorage.removeItem(LOCAL_STORAGE_LAST_CAMERA_ID_KEY);
+
     const qrCodeSpy = spyOn(Html5Qrcode, 'getCameras').and.returnValue(of(testCameras).toPromise());
 
     const service = new QRCodeReaderService();
@@ -20,6 +23,19 @@ describe('QRCodeReaderService', () => {
 
     expect(qrCodeSpy).toHaveBeenCalled();
     expect(cameras).toEqual(testCameras);
+  });
+
+  it('lastUsedCameraId saved and loaded successfully', () => {
+    const {service} = setup();
+    const testCameraId = '123';
+
+    expect(localStorage.getItem(LOCAL_STORAGE_LAST_CAMERA_ID_KEY)).toBe(null);
+
+    service.saveLastUsedCameraId(testCameraId);
+
+    expect(service.getLastUsedCameraId).toBe(testCameraId);
+
+    expect(localStorage.getItem(LOCAL_STORAGE_LAST_CAMERA_ID_KEY)).toBe(testCameraId);
   });
 
 });
