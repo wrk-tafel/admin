3d438c7635c8665b7aedbec8224d48e3579c28ca
Stephan Prantl <stephan.prantl@gmail.com>
2022-11-10 18:37:06 +0100

    fixed testconfig

--- frontend/src/main/webapp/src/app/modules/checkin/views/scanner/scanner.component.spec.ts
+++ frontend/src/main/webapp/src/app/modules/checkin/views/scanner/scanner.component.spec.ts
@@ -1,12 +1,33 @@
 import {TestBed, waitForAsync} from '@angular/core/testing';
 import {CommonModule} from '@angular/common';
 import {ScannerComponent} from "./scanner.component";
+import {ScannerApiService} from "../../api/scanner-api.service";
+import {QRCodeReaderService} from "./camera/qrcode-reader.service";
 
 describe('ScannerComponent', () => {
+  let apiService: jasmine.SpyObj<ScannerApiService>;
+  let qrCodeReaderService: jasmine.SpyObj<QRCodeReaderService>;
+
   beforeEach(waitForAsync(() => {
+    const apiServiceSpy = jasmine.createSpyObj('ScannerApiService', ['TODO']);
+    const qrCodeReaderServiceSpy = jasmine.createSpyObj('QRCodeReaderService', ['TODO']);
+
     TestBed.configureTestingModule({
-      imports: [CommonModule]
+      imports: [CommonModule],
+      providers: [
+        {
+          provide: ScannerApiService,
+          useValue: apiServiceSpy
+        },
+        {
+          provide: QRCodeReaderService,
+          useValue: qrCodeReaderServiceSpy
+        }
+      ]
     }).compileComponents();
+
+    apiService = TestBed.inject(ScannerApiService) as jasmine.SpyObj<ScannerApiService>;
+    qrCodeReaderService = TestBed.inject(QRCodeReaderService) as jasmine.SpyObj<QRCodeReaderService>;
   }));
 
   it('component can be created', () => {
