30758c1e847e95e8ac3686a54afdd08a3afd36e7
Stephan Prantl <stephan.prantl@gmail.com>
2022-11-18 00:06:15 +0100

    fixed data

--- backend/src/main/resources/db-migration-testdata/testdata.sql
+++ backend/src/main/resources/db-migration-testdata/testdata.sql
@@ -65,14 +65,14 @@ INSERT INTO customers (id, created_at, updated_at, customer_id, user_id, firstna
 values (1, NOW(), NOW(), 101, 100, 'Eva', 'Musterfrau', '1990-01-01', 2, 'Erdberg', 2, '1', '20', '1010', 'Wien',
         '00436645678953', 'eva.musterfrau@wrk.at', 'Rotes Kreuz Wien', 456.00, '2999-12-31', '2999-12-31');
 INSERT INTO customers_addpersons (id, created_at, updated_at, customer_id, firstname, lastname, birth_date, income,
-                                  income_due)
-values (1, NOW(), NOW(), 1, 'Child 1', 'Musterfrau', '2000-01-01', 500, '2999-12-31');
+                                  income_due, country_id)
+values (1, NOW(), NOW(), 1, 'Child 1', 'Musterfrau', '2000-01-01', 500, '2999-12-31', 1);
 INSERT INTO customers_addpersons (id, created_at, updated_at, customer_id, firstname, lastname, birth_date, income,
-                                  income_due)
-values (2, NOW(), NOW(), 1, 'Child 2', 'Musterfrau', '2020-01-01', 0, null);
+                                  income_due, country_id)
+values (2, NOW(), NOW(), 1, 'Child 2', 'Musterfrau', '2020-01-01', 0, null, 1);
 INSERT INTO customers_addpersons (id, created_at, updated_at, customer_id, firstname, lastname, birth_date, income,
-                                  income_due)
-values (3, NOW(), NOW(), 1, 'Child 3', 'Musterfrau', '2020-01-01', null, null);
+                                  income_due, country_id)
+values (3, NOW(), NOW(), 1, 'Child 3', 'Musterfrau', '2020-01-01', null, null, 1);
 INSERT INTO customers (id, created_at, updated_at, customer_id, user_id, firstname, lastname, birth_date, country_id,
                        address_street, address_houseNumber, address_stairway, address_door, address_postalCode,
                        address_city, telephone_number, email, employer, income, income_due, valid_until)
--- backend/src/main/resources/db-migration/R__00007_addpersons_add_country.sql
+++ backend/src/main/resources/db-migration/R__00007_addpersons_add_country.sql
@@ -1,2 +1,2 @@
 alter table if exists customers_addpersons
-    add column if not exists country_id bigint null REFERENCES static_countries (id);
+    add column if not exists country_id bigint not null REFERENCES static_countries (id);
