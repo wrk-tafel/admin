5737512d26ce9db5e779602fb1bc8f3fe654d169
Stephan Pantl <stephan.prantl@gmail.com>
2023-01-01 16:31:12 +0100

    fixed cors

--- backend/src/main/kotlin/at/wrk/tafel/admin/backend/config/WebSocketSecurityConfig.kt
+++ backend/src/main/kotlin/at/wrk/tafel/admin/backend/config/WebSocketSecurityConfig.kt
@@ -4,16 +4,21 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.Message
 import org.springframework.security.authorization.AuthorizationManager
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer
 import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity
 import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager
 
 @Configuration
 @EnableWebSocketSecurity
-class WebSocketSecurityConfig {
+class WebSocketSecurityConfig : AbstractSecurityWebSocketMessageBrokerConfigurer() {
 
     @Bean
     fun authorizationManager(messages: MessageMatcherDelegatingAuthorizationManager.Builder): AuthorizationManager<Message<*>?>? {
         return messages.anyMessage().permitAll().build()
+    }
+
+    override fun sameOriginDisabled(): Boolean {
+        return true
     }
 
 }
