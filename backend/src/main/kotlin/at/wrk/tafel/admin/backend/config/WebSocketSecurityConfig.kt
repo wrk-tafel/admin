8bcdab6b339ec1a058dba4327fb4b6da96cd7d21
Stephan Pantl <stephan.prantl@gmail.com>
2023-01-01 15:43:35 +0100

    replaced deprecated

--- backend/src/main/kotlin/at/wrk/tafel/admin/backend/config/WebSocketSecurityConfig.kt
+++ backend/src/main/kotlin/at/wrk/tafel/admin/backend/config/WebSocketSecurityConfig.kt
@@ -1,20 +1,19 @@
 package at.wrk.tafel.admin.backend.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry
-import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer
+import org.springframework.messaging.Message
+import org.springframework.security.authorization.AuthorizationManager
+import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity
+import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager
 
 @Configuration
-class WebSocketSecurityConfig : AbstractSecurityWebSocketMessageBrokerConfigurer() {
+@EnableWebSocketSecurity
+class WebSocketSecurityConfig {
 
-    override fun configureInbound(messages: MessageSecurityMetadataSourceRegistry) {
-        // TODO set permissions
-        messages.anyMessage().permitAll()
-    }
-
-    override fun sameOriginDisabled(): Boolean {
-        // TODO fix
-        return true
+    @Bean
+    fun authorizationManager(messages: MessageMatcherDelegatingAuthorizationManager.Builder): AuthorizationManager<Message<*>?>? {
+        return messages.anyMessage().authenticated().build()
     }
 
 }
