583b3e1ef2fd93d82a04522207344c1a17bd9c41
Stephan Pantl <stephan.prantl@gmail.com>
2023-01-05 00:51:35 +0100

    finished converter

--- backend/src/main/kotlin/at/wrk/tafel/admin/backend/common/auth/components/TafelJwtAuthConverter.kt
+++ backend/src/main/kotlin/at/wrk/tafel/admin/backend/common/auth/components/TafelJwtAuthConverter.kt
@@ -1,24 +1,25 @@
 package at.wrk.tafel.admin.backend.common.auth.components
 
+import at.wrk.tafel.admin.backend.common.auth.model.TafelJwtAuthenticationToken
 import jakarta.servlet.http.HttpServletRequest
+import org.springframework.http.HttpHeaders
 import org.springframework.security.core.Authentication
 import org.springframework.security.web.authentication.AuthenticationConverter
 
-class TafelJwtAuthConverter(
-    private val jwtTokenService: JwtTokenService
-) : AuthenticationConverter {
+class TafelJwtAuthConverter : AuthenticationConverter {
 
     companion object {
         private const val headerPrefix = "Bearer"
     }
 
     override fun convert(request: HttpServletRequest): Authentication? {
-        val header = request.getHeader("Authorization")
+        val tokenString = request.getHeader(HttpHeaders.AUTHORIZATION)
             ?.takeIf { it.startsWith(headerPrefix) }
+            ?.removePrefix(headerPrefix)
             ?.trim()
 
-        if (header != null) {
-            
+        if (tokenString?.isNotBlank() == true) {
+            return TafelJwtAuthenticationToken(tokenString)
         }
 
         return null
---
+++ backend/src/test/kotlin/at/wrk/tafel/admin/backend/common/auth/components/TafelJwtAuthConverterTest.kt
@@ -0,0 +1,51 @@
+package at.wrk.tafel.admin.backend.common.auth.components
+
+import at.wrk.tafel.admin.backend.common.auth.model.TafelJwtAuthenticationToken
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.junit5.MockKExtension
+import jakarta.servlet.http.HttpServletRequest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.http.HttpHeaders
+
+@ExtendWith(MockKExtension::class)
+internal class TafelJwtAuthConverterTest {
+
+    @RelaxedMockK
+    private lateinit var request: HttpServletRequest
+
+    @InjectMockKs
+    private lateinit var converter: TafelJwtAuthConverter
+
+    @Test
+    fun `convert - missing header`() {
+        every { request.getHeader(HttpHeaders.AUTHORIZATION) } returns null
+
+        val result = converter.convert(request)
+
+        assertThat(result).isNull()
+    }
+
+    @Test
+    fun `convert - header with invalid prefix`() {
+        every { request.getHeader(HttpHeaders.AUTHORIZATION) } returns "WRONG TEST123"
+
+        val result = converter.convert(request)
+
+        assertThat(result).isNull()
+    }
+
+    @Test
+    fun `convert - header valid`() {
+        val token = "TEST123"
+        every { request.getHeader(HttpHeaders.AUTHORIZATION) } returns "Bearer $token"
+
+        val result = converter.convert(request) as TafelJwtAuthenticationToken
+
+        assertThat(result.tokenValue).isEqualTo(token)
+    }
+
+}
