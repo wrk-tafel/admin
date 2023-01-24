da88a98592622b9d4782deea50bd9bf960304c22
Stephan Pantl <stephan.prantl@gmail.com>
2023-01-24 23:50:53 +0100

    added auth handler

---
+++ backend/src/main/kotlin/at/wrk/tafel/admin/backend/common/auth/websocket/TafelWSJwtAuthHandshakeHandler.kt
@@ -0,0 +1,40 @@
+package at.wrk.tafel.admin.backend.common.auth.websocket
+
+import at.wrk.tafel.admin.backend.common.auth.components.TafelJwtAuthConverter
+import at.wrk.tafel.admin.backend.common.auth.components.TafelJwtAuthProvider
+import org.springframework.http.server.ServerHttpRequest
+import org.springframework.http.server.ServerHttpResponse
+import org.springframework.http.server.ServletServerHttpRequest
+import org.springframework.web.socket.WebSocketHandler
+import org.springframework.web.socket.server.HandshakeInterceptor
+import java.lang.Exception
+
+class TafelWSJwtAuthHandshakeHandler(
+    private val authConverter: TafelJwtAuthConverter,
+    private val authProvider: TafelJwtAuthProvider
+) : HandshakeInterceptor {
+
+    override fun beforeHandshake(
+        request: ServerHttpRequest,
+        response: ServerHttpResponse,
+        wsHandler: WebSocketHandler,
+        attributes: MutableMap<String, Any>
+    ): Boolean {
+        val servletRequest = (request as ServletServerHttpRequest).servletRequest
+        val authentication = authConverter.convert(servletRequest)
+
+        return authentication?.let {
+            val authenticationResult = authProvider.authenticate(it)
+            return authenticationResult.isAuthenticated
+        } ?: false
+    }
+
+    override fun afterHandshake(
+        request: ServerHttpRequest,
+        response: ServerHttpResponse,
+        wsHandler: WebSocketHandler,
+        exception: Exception?
+    ) {
+    }
+
+}
--- backend/src/main/kotlin/at/wrk/tafel/admin/backend/config/WebSocketAndSecurityConfig.kt
+++ backend/src/main/kotlin/at/wrk/tafel/admin/backend/config/WebSocketAndSecurityConfig.kt
@@ -1,22 +1,31 @@
 package at.wrk.tafel.admin.backend.config
 
 import at.wrk.tafel.admin.backend.common.ExcludeFromTestCoverage
+import at.wrk.tafel.admin.backend.common.auth.components.TafelJwtAuthConverter
+import at.wrk.tafel.admin.backend.common.auth.components.TafelJwtAuthProvider
+import at.wrk.tafel.admin.backend.common.auth.websocket.TafelWSJwtAuthHandshakeHandler
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+import org.springframework.web.socket.config.annotation.*
 
 @Configuration
 @EnableWebSocketMessageBroker
 @ExcludeFromTestCoverage
-class WebSocketAndSecurityConfig : WebSocketMessageBrokerConfigurer {
+class WebSocketAndSecurityConfig(
+    private val authConverter: TafelJwtAuthConverter,
+    private val authProvider: TafelJwtAuthProvider
+) : WebSocketMessageBrokerConfigurer {
 
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
         registry.addEndpoint("/api/websockets")
             // TODO same-origin should be used (probably only possible with sockjs) while documentation mentions this to be default also without sockjs
             // TODO while with enabled authentication and the strict cookie it should be also safe to have cors disabled
             .setAllowedOrigins("*")
+            .addInterceptors(
+                TafelWSJwtAuthHandshakeHandler(
+                    authConverter, authProvider
+                )
+            )
     }
 
     override fun configureMessageBroker(registry: MessageBrokerRegistry) {
