9abe708ebe4a6bac34a777354af16dd119e155c4
Stephan Pantl <stephan.prantl@gmail.com>
2023-02-01 23:50:14 +0100

    added service and scanner registry

--- backend/src/main/kotlin/at/wrk/tafel/admin/backend/modules/checkin/scanner/ScannerController.kt
+++ backend/src/main/kotlin/at/wrk/tafel/admin/backend/modules/checkin/scanner/ScannerController.kt
@@ -1,28 +1,29 @@
 package at.wrk.tafel.admin.backend.modules.checkin.scanner
 
 import at.wrk.tafel.admin.backend.common.ExcludeFromTestCoverage
+import at.wrk.tafel.admin.backend.common.auth.model.TafelJwtAuthentication
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.simp.annotation.SendToUser
-import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.stereotype.Controller
 import java.util.*
-import kotlin.math.abs
 
 @Controller
-@EnableScheduling
-class ScannerController {
+class ScannerController(
+    private val scannerService: ScannerService
+) {
 
     private val logger: Logger = LoggerFactory.getLogger(ScannerController::class.java)
 
     @MessageMapping("/scanners/register")
     @SendToUser("/queue/scanners/registration")
-    fun registerScanner(): ScannerRegistration {
-        val scannerId = abs(Random().nextInt()) // TODO generate id (atomic sequence)
-        logger.info("Scanner registered - ID: $scannerId")
-
-        return ScannerRegistration(scannerId = scannerId)
+    fun registerScanner(
+        @AuthenticationPrincipal authentication: TafelJwtAuthentication
+    ): ScannerRegistration {
+        val id = scannerService.registerScanner(authentication.username!!)
+        return ScannerRegistration(scannerId = id)
     }
 
     @MessageMapping("/scanners/result")
---
+++ backend/src/main/kotlin/at/wrk/tafel/admin/backend/modules/checkin/scanner/ScannerService.kt
@@ -0,0 +1,30 @@
+package at.wrk.tafel.admin.backend.modules.checkin.scanner
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ConcurrentMap
+import java.util.concurrent.atomic.AtomicInteger
+
+@Service
+class ScannerService {
+
+    private val logger: Logger = LoggerFactory.getLogger(ScannerService::class.java)
+
+    private val registrationMap: ConcurrentMap<String, Int> = ConcurrentHashMap()
+    private val registeredClientsCount: AtomicInteger = AtomicInteger()
+
+    fun registerScanner(username: String): Int {
+        var id = registrationMap[username]
+        if (id == null) {
+            id = registeredClientsCount.incrementAndGet()
+            registrationMap[username] = id
+            logger.info("Scanner for user $username registered with ID: $id")
+        } else {
+            logger.info("Scanner for user $username re-assigned to ID: $id")
+        }
+        return id
+    }
+
+}
