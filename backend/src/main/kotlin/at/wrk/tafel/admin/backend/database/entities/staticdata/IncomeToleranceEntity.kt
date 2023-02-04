757e39b8d38a5d56a222381634382627e39e04aa
Stephan Pantl <stephan.prantl@gmail.com>
2023-02-05 00:18:55 +0100

    excluded entities

--- backend/src/main/kotlin/at/wrk/tafel/admin/backend/database/entities/staticdata/IncomeToleranceEntity.kt
+++ backend/src/main/kotlin/at/wrk/tafel/admin/backend/database/entities/staticdata/IncomeToleranceEntity.kt
@@ -2,7 +2,9 @@ package at.wrk.tafel.admin.backend.database.entities.staticdata
 
 import jakarta.persistence.DiscriminatorValue
 import jakarta.persistence.Entity
+import at.wrk.tafel.admin.backend.common.ExcludeFromTestCoverage
 
 @Entity(name = "IncomeTolerance")
 @DiscriminatorValue("INCOME-TOLERANCE")
+@ExcludeFromTestCoverage
 class IncomeToleranceEntity : StaticValueEntity()
