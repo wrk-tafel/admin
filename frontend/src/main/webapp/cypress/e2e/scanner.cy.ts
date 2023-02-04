be06c26a1a4d5798537acdc50c2b51d416e573a6
Stephan Pantl <stephan.prantl@gmail.com>
2023-02-05 00:44:59 +0100

    raised wait time

--- frontend/src/main/webapp/cypress/e2e/scanner.cy.ts
+++ frontend/src/main/webapp/cypress/e2e/scanner.cy.ts
@@ -8,7 +8,7 @@ describe('Scanner', () => {
     cy.visit('/#/anmeldung/scanner');
 
     // to be safe having the ws-connection established and the webcam ready
-    cy.wait(250);
+    cy.wait(2000);
 
     cy.byTestId('state-server').should('have.class', 'badge-success');
     cy.byTestId('state-camera').should('have.class', 'badge-success');
