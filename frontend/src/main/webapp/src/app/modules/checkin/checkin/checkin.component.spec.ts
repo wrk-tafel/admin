eb3ca2fca4c23cd9925209405e8820e0ac0d95ed
Stephan Pantl <stephan.prantl@gmail.com>
2023-02-05 18:15:28 +0100

    fixed test

--- frontend/src/main/webapp/src/app/modules/checkin/checkin/checkin.component.spec.ts
+++ frontend/src/main/webapp/src/app/modules/checkin/checkin/checkin.component.spec.ts
@@ -1,3 +1,12 @@
+import {TestBed} from '@angular/core/testing';
+import {CheckinComponent} from './checkin.component';
+
 describe('CheckinComponent', () => {
+
+  it('component can be created', () => {
+    const fixture = TestBed.createComponent(CheckinComponent);
+    const component = fixture.componentInstance;
+    expect(component).toBeTruthy();
+  });
 
 });
