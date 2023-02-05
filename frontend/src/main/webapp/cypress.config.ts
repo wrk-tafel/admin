574ffdf7773865a5e7c2717665b8e05f5429a707
Stephan Pantl <stephan.prantl@gmail.com>
2023-02-05 01:05:11 +0100

    fixed e2e

--- frontend/src/main/webapp/cypress.config.ts
+++ frontend/src/main/webapp/cypress.config.ts
@@ -5,6 +5,11 @@ export default defineConfig({
   viewportWidth: 1280,
   viewportHeight: 1024,
   videoCompression: false,
+  env: {
+    'browserPermissions': {
+      'camera': 'allow'
+    }
+  },
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
@@ -31,7 +36,7 @@ export default defineConfig({
         return launchOptions;
       });
 
-      return require('./cypress/plugins/index.js')(on, config);
+      return require('./cypress/plugins/index.ts')(on, config);
     },
     baseUrl: 'http://localhost:4200/'
   },
