a93b786d3ba0ce223dc77ad16995afed90bae6a3
Stephan Pantl <stephan.prantl@gmail.com>
2023-02-22 19:08:02 +0100

    added cypress test

--- frontend/src/main/webapp/cypress/e2e/dashboard.cy.ts
+++ frontend/src/main/webapp/cypress/e2e/dashboard.cy.ts
@@ -5,8 +5,22 @@ describe('Dashboard', () => {
     cy.visit('/#/');
   });
 
-  it('TODO', () => {
-    // TODO impl
+  it('start and stop distribution', () => {
+    cy.byTestId('distribtion-state-text').should('have.text', 'Ausgabe INAKTIV');
+
+    cy.byTestId('distribution-start-button').click();
+
+    cy.byTestId('distribtion-state-text').should('have.text', 'Ausgabe AKTIV');
+
+    cy.byTestId('distribution-stop-button').click();
+
+    cy.byTestId('distribution-stop-modal-cancel-button').click();
+
+    cy.byTestId('distribution-stop-button').click();
+
+    cy.byTestId('distribution-stop-modal-ok-button').click();
+
+    cy.byTestId('distribtion-state-text').should('have.text', 'Ausgabe INAKTIV');
   });
 
 });
