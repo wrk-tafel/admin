f4ecb45a6fc2d9391a18f2b8f501bc011c19503e
Stephan Pantl <stephan.prantl@gmail.com>
2023-03-18 13:15:24 +0100

    splitted testdata

--- frontend/src/main/webapp/cypress/e2e/customer-edit.cy.ts
+++ frontend/src/main/webapp/cypress/e2e/customer-edit.cy.ts
@@ -5,15 +5,15 @@ describe('Customer Edit', () => {
   });
 
   it('customerId correct', () => {
-    cy.visit('/#/kunden/bearbeiten/101');
-    cy.byTestId('customerIdInput').should('have.value', '101');
+    cy.visit('/#/kunden/bearbeiten/102');
+    cy.byTestId('customerIdInput').should('have.value', '102');
   });
 
   it('customer updated', () => {
-    cy.visit('/#/kunden/bearbeiten/101');
+    cy.visit('/#/kunden/bearbeiten/102');
     cy.byTestId('save-button').click();
 
-    cy.url().should('contain', '/kunden/detail/101');
+    cy.url().should('contain', '/kunden/detail/102');
 
     // TODO change actual data (but for that create a new dedicated customer beforehand)
   });
