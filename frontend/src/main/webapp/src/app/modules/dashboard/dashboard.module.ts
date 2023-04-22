4f73ac8d3ec85e8941dd88a1861b99f9b66ec0ef
Stephan Pantl <stephan.prantl@gmail.com>
2023-04-22 23:08:22 +0200

    adapted dashboard and re-enabled bootstrap

--- frontend/src/main/webapp/src/app/modules/dashboard/dashboard.module.ts
+++ frontend/src/main/webapp/src/app/modules/dashboard/dashboard.module.ts
@@ -6,30 +6,36 @@ import {TafelCommonModule} from '../../common/tafel-common.module';
 import {DistributionStateComponent} from './components/distribution-state/distribution-state.component';
 import {RegisteredCustomersComponent} from './components/registered-customers/registered-customers.component';
 import {
-    ButtonModule,
-    CardBodyComponent, CardComponent, CardGroupComponent, ColComponent,
-    ModalModule,
-    ProgressBarComponent,
-    ProgressComponent,
-    ProgressModule, RowComponent
+  ButtonModule,
+  CardBodyComponent,
+  CardComponent,
+  CardGroupComponent,
+  CardHeaderComponent,
+  ColComponent,
+  ModalModule,
+  ProgressBarComponent,
+  ProgressComponent,
+  ProgressModule,
+  RowComponent
 } from '@coreui/angular';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        TafelCommonModule,
-        DashboardRoutingModule,
-        ButtonModule,
-        ModalModule,
-        ProgressModule,
-        ProgressBarComponent,
-        ProgressComponent,
-        CardBodyComponent,
-        CardComponent,
-        CardGroupComponent,
-        ColComponent,
-        RowComponent
-    ],
+  imports: [
+    CommonModule,
+    TafelCommonModule,
+    DashboardRoutingModule,
+    ButtonModule,
+    ModalModule,
+    ProgressModule,
+    ProgressBarComponent,
+    ProgressComponent,
+    CardComponent,
+    CardBodyComponent,
+    CardHeaderComponent,
+    CardGroupComponent,
+    ColComponent,
+    RowComponent
+  ],
   declarations: [
     DashboardComponent,
     DistributionStateComponent,
