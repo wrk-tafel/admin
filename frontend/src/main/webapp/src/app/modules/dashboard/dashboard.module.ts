fc0dd41fcb5d09b641448aac46548565165c0ad5
Stephan Pantl <stephan.prantl@gmail.com>
2023-04-22 21:44:31 +0200

    improved

--- frontend/src/main/webapp/src/app/modules/dashboard/dashboard.module.ts
+++ frontend/src/main/webapp/src/app/modules/dashboard/dashboard.module.ts
@@ -5,19 +5,31 @@ import {CommonModule} from '@angular/common';
 import {TafelCommonModule} from '../../common/tafel-common.module';
 import {DistributionStateComponent} from './components/distribution-state/distribution-state.component';
 import {RegisteredCustomersComponent} from './components/registered-customers/registered-customers.component';
-import {ButtonModule, ModalModule, ProgressBarComponent, ProgressComponent, ProgressModule} from '@coreui/angular';
+import {
+    ButtonModule,
+    CardBodyComponent, CardComponent, CardGroupComponent, ColComponent,
+    ModalModule,
+    ProgressBarComponent,
+    ProgressComponent,
+    ProgressModule, RowComponent
+} from '@coreui/angular';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    TafelCommonModule,
-    DashboardRoutingModule,
-    ButtonModule,
-    ModalModule,
-    ProgressModule,
-    ProgressBarComponent,
-    ProgressComponent
-  ],
+    imports: [
+        CommonModule,
+        TafelCommonModule,
+        DashboardRoutingModule,
+        ButtonModule,
+        ModalModule,
+        ProgressModule,
+        ProgressBarComponent,
+        ProgressComponent,
+        CardBodyComponent,
+        CardComponent,
+        CardGroupComponent,
+        ColComponent,
+        RowComponent
+    ],
   declarations: [
     DashboardComponent,
     DistributionStateComponent,
