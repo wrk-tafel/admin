ded01d746e796bebe5bf64129620c4f99b8e97f9
Stephan Pantl <stephan.prantl@gmail.com>
2023-04-24 00:20:19 +0200

    fixed test

--- frontend/src/main/webapp/src/app/common/views/default-layout/default-header/default-header.component.spec.ts
+++ frontend/src/main/webapp/src/app/common/views/default-layout/default-header/default-header.component.spec.ts
@@ -13,11 +13,15 @@ import {
 import {IconSetService} from '@coreui/icons-angular';
 import {DefaultHeaderComponent} from './default-header.component';
 import {RouterTestingModule} from '@angular/router/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {AuthenticationService} from '../../../security/authentication.service';
+import {of} from 'rxjs';
 
 describe('DefaultHeaderComponent', () => {
   let component: DefaultHeaderComponent;
   let fixture: ComponentFixture<DefaultHeaderComponent>;
   let iconSetService: IconSetService;
+  let authenticationService: jasmine.SpyObj<AuthenticationService>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -30,12 +34,21 @@ describe('DefaultHeaderComponent', () => {
         AvatarModule,
         DropdownModule,
         BreadcrumbModule,
+        HttpClientTestingModule,
         RouterTestingModule,
         SidebarModule
       ],
-      providers: [IconSetService]
+      providers: [
+        IconSetService,
+        {
+          provide: AuthenticationService,
+          useValue: jasmine.createSpyObj('AuthenticationService', ['logout', 'redirectToLogin'])
+        }
+      ]
     })
       .compileComponents();
+
+    authenticationService = TestBed.inject(AuthenticationService) as jasmine.SpyObj<AuthenticationService>;
   });
 
   beforeEach(() => {
@@ -48,6 +61,14 @@ describe('DefaultHeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('logout', () => {
+    authenticationService.logout.and.returnValues(of(null));
+
+    component.logout();
+
+    expect(authenticationService.redirectToLogin).toHaveBeenCalled();
   });
 
 });
