398e08854aeff1960851ead6ed360ebc5fbdffb6
Stephan Pantl <stephan.prantl@gmail.com>
2023-04-25 18:19:14 +0200

    basic impl

--- frontend/src/main/webapp/src/app/modules/user/user-passwordchange/user-passwordchange.component.ts
+++ frontend/src/main/webapp/src/app/modules/user/user-passwordchange/user-passwordchange.component.ts
@@ -1,8 +1,23 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {PasswordChangeFormComponent} from '../../../common/views/passwordchange-form/passwordchange-form.component';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'tafel-user-passwordchange',
   templateUrl: 'user-passwordchange.component.html'
 })
 export class UserPasswordChangeComponent {
+  @ViewChild(PasswordChangeFormComponent) public form: PasswordChangeFormComponent;
+
+  constructor(private router: Router) {
+  }
+
+  changePassword() {
+    this.form.changePassword().subscribe();
+  }
+
+  isSaveDisabled(): Boolean {
+    return !this.form?.form.valid;
+  }
+
 }
