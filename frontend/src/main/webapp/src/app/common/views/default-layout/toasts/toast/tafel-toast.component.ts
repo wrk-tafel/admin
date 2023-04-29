9a6a5a2fa2ee4e23971c8aca6c651f92ea0f24e1
Stephan Pantl <stephan.prantl@gmail.com>
2023-04-29 21:46:53 +0200

    improved

--- frontend/src/main/webapp/src/app/common/views/default-layout/toasts/toast/tafel-toast.component.ts
+++ frontend/src/main/webapp/src/app/common/views/default-layout/toasts/toast/tafel-toast.component.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
+import {ToastComponent} from '@coreui/angular';
 
 @Component({
   selector: 'tafel-toast',
   templateUrl: 'tafel-toast.component.html'
 })
-export class TafelToastComponent {
+export class TafelToastComponent extends ToastComponent {
   bgColor: string;
   titlePrefix: string;
   title: string;
