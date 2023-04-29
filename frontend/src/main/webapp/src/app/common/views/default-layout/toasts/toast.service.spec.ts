bc3144ca1f9f642b40f2813a9e2072a591f34c66
Stephan Pantl <stephan.prantl@gmail.com>
2023-04-29 03:23:09 +0200

    added test

--- frontend/src/main/webapp/src/app/common/views/default-layout/toasts/toast.service.spec.ts
+++ frontend/src/main/webapp/src/app/common/views/default-layout/toasts/toast.service.spec.ts
@@ -1,20 +1,29 @@
 import {TestBed} from '@angular/core/testing';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
-import {ToastService} from './toast.service';
+import {ToastOptions, ToastService, ToastType} from './toast.service';
 
 describe('ToastService', () => {
+  let service: ToastService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      declarations: [
+      providers: [
         ToastService
       ]
     });
+
+    service = TestBed.inject(ToastService);
   });
 
-  it('TODO', () => {
-    expect(true).toBeFalse();
+  it('showToast publishes to subject', () => {
+    const expectedOptions: ToastOptions = {type: ToastType.INFO, title: 'title-123', message: 'message-123'};
+
+    service.addToastSubject.subscribe((options: ToastOptions) => {
+      expect(options).toEqual(options);
+    });
+
+    service.showToast(expectedOptions);
   });
 
 });
