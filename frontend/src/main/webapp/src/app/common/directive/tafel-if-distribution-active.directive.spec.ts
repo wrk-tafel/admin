ede49b271c8b1d7184065343e102faf3a520a1c2
Stephan Pantl <stephan.prantl@gmail.com>
2023-05-01 21:28:36 +0200

    add custom directive

---
+++ frontend/src/main/webapp/src/app/common/directive/tafel-if-distribution-active.directive.spec.ts
@@ -0,0 +1,42 @@
+import {TafelIfDistributionActiveDirective} from './tafel-if-distribution-active.directive';
+import {BehaviorSubject} from 'rxjs';
+import {DistributionItem} from '../../api/distribution-api.service';
+
+describe('TafelIfDistributionActiveDirective', () => {
+
+  function setup() {
+    const viewContainerSpy =
+      jasmine.createSpyObj('ViewContainer', ['createEmbeddedView', 'clear']);
+    const globalStateServiceSpy =
+      jasmine.createSpyObj('GlobalStateService', ['getCurrentDistribution']);
+    const directive = new TafelIfDistributionActiveDirective(undefined, viewContainerSpy, globalStateServiceSpy);
+    return {viewContainerSpy, globalStateServiceSpy, directive};
+  }
+
+  it('should render when distribution is active', () => {
+    const {viewContainerSpy, globalStateServiceSpy, directive} = setup();
+    const mockDistributionItem: DistributionItem = {
+      id: 123,
+      state: {
+        name: 'OPEN',
+        stateLabel: 'Offen',
+        actionLabel: 'Offen'
+      }
+    };
+    globalStateServiceSpy.getCurrentDistribution.and.returnValue(new BehaviorSubject(mockDistributionItem));
+
+    directive.ngAfterViewInit();
+
+    expect(viewContainerSpy.createEmbeddedView).toHaveBeenCalled();
+  });
+
+  it('should not render distribution is inactive', () => {
+    const {viewContainerSpy, globalStateServiceSpy, directive} = setup();
+    globalStateServiceSpy.getCurrentDistribution.and.returnValue(new BehaviorSubject(undefined));
+
+    directive.ngAfterViewInit();
+
+    expect(viewContainerSpy.clear).toHaveBeenCalled();
+  });
+
+});
---
+++ frontend/src/main/webapp/src/app/common/directive/tafel-if-distribution-active.directive.ts
@@ -0,0 +1,26 @@
+import {AfterViewInit, Directive, TemplateRef, ViewContainerRef} from '@angular/core';
+import {GlobalStateService} from '../state/global-state.service';
+import {DistributionItem} from "../../api/distribution-api.service";
+
+@Directive({
+  selector: '[tafelIfDistributionActive]'
+})
+export class TafelIfDistributionActiveDirective implements AfterViewInit {
+  constructor(
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+    private templateRef: TemplateRef<any>,
+    private viewContainer: ViewContainerRef,
+    private globalStateService: GlobalStateService) {
+  }
+
+  ngAfterViewInit(): void {
+    this.globalStateService.getCurrentDistribution().subscribe((distributionItem: DistributionItem) => {
+      if (distributionItem) {
+        this.viewContainer.createEmbeddedView(this.templateRef);
+      } else {
+        this.viewContainer.clear();
+      }
+    });
+  }
+
+}
