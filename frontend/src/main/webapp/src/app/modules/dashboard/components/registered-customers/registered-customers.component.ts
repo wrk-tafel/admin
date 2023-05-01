b431f8a7897e3dbafafb1285a2a94524e95bc543
Stephan Pantl <stephan.prantl@gmail.com>
2023-05-01 21:40:37 +0200

    added button and api call

--- frontend/src/main/webapp/src/app/modules/dashboard/components/registered-customers/registered-customers.component.ts
+++ frontend/src/main/webapp/src/app/modules/dashboard/components/registered-customers/registered-customers.component.ts
@@ -1,4 +1,7 @@
 import {Component, Input} from '@angular/core';
+import {DistributionApiService} from '../../../../api/distribution-api.service';
+import {HttpResponse} from '@angular/common/http';
+import {FileHelperService} from '../../../../common/util/file-helper.service';
 
 @Component({
   selector: 'tafel-registered-customers',
@@ -8,5 +11,22 @@ import {Component, Input} from '@angular/core';
 export class RegisteredCustomersComponent {
 
   @Input() count?: number;
+
+  constructor(
+    private distributionApiService: DistributionApiService,
+    private fileHelperService: FileHelperService
+  ) {
+  }
+
+  downloadCustomerList() {
+    this.distributionApiService.downloadCustomerList().subscribe(response => this.processPdfResponse(response));
+  }
+
+  private processPdfResponse(response: HttpResponse<ArrayBuffer>) {
+    const contentDisposition = response.headers.get('content-disposition');
+    const filename = contentDisposition.split(';')[1].split('filename')[1].split('=')[1].trim();
+    const data = new Blob([response.body], {type: 'application/pdf'});
+    this.fileHelperService.downloadFile(filename, data);
+  }
 
 }
