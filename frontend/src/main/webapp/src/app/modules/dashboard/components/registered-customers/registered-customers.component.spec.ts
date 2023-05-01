09d1ac4fd027e3ce68911acec9cf133309370f19
Stephan Pantl <stephan.prantl@gmail.com>
2023-05-02 00:26:47 +0200

    fixed test

--- frontend/src/main/webapp/src/app/modules/dashboard/components/registered-customers/registered-customers.component.spec.ts
+++ frontend/src/main/webapp/src/app/modules/dashboard/components/registered-customers/registered-customers.component.spec.ts
@@ -3,12 +3,17 @@ import {RouterTestingModule} from '@angular/router/testing';
 import {RegisteredCustomersComponent} from './registered-customers.component';
 import {By} from '@angular/platform-browser';
 import {CardModule, ColComponent, ModalModule, RowComponent} from '@coreui/angular';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {DistributionApiService} from "../../../../api/distribution-api.service";
+import {EMPTY} from "rxjs";
 
 describe('RegisteredCustomersComponent', () => {
+  let distributionApiService: jasmine.SpyObj<DistributionApiService>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
+        HttpClientTestingModule,
         RouterTestingModule,
         ModalModule,
         CardModule,
@@ -17,8 +22,16 @@ describe('RegisteredCustomersComponent', () => {
       ],
       declarations: [
         RegisteredCustomersComponent
+      ],
+      providers: [
+        {
+          provide: DistributionApiService,
+          useValue: jasmine.createSpyObj('DistributionApiService', ['downloadCustomerList'])
+        },
       ]
     }).compileComponents();
+
+    distributionApiService = TestBed.inject(DistributionApiService) as jasmine.SpyObj<DistributionApiService>;
   }));
 
   it('component can be created', () => {
@@ -36,6 +49,17 @@ describe('RegisteredCustomersComponent', () => {
 
     fixture.detectChanges();
     expect(fixture.debugElement.query(By.css('[testid="customers-count"]')).nativeElement.textContent).toBe(`${count}`);
+  });
+
+  it('download customers list', () => {
+    const fixture = TestBed.createComponent(RegisteredCustomersComponent);
+    const component = fixture.componentInstance;
+    fixture.detectChanges();
+    distributionApiService.downloadCustomerList.and.returnValue(EMPTY);
+
+    component.downloadCustomerList();
+
+    expect(distributionApiService.downloadCustomerList).toHaveBeenCalled();
   });
 
 });
