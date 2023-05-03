2d04be6057e1a24f4bb068f96d6daf5e2acd5624
Stephan Pantl <stephan.prantl@gmail.com>
2023-05-03 23:21:22 +0200

    enhanced test

--- frontend/src/main/webapp/src/app/modules/dashboard/components/registered-customers/registered-customers.component.spec.ts
+++ frontend/src/main/webapp/src/app/modules/dashboard/components/registered-customers/registered-customers.component.spec.ts
@@ -4,11 +4,14 @@ import {RegisteredCustomersComponent} from './registered-customers.component';
 import {By} from '@angular/platform-browser';
 import {CardModule, ColComponent, ModalModule, RowComponent} from '@coreui/angular';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
-import {DistributionApiService} from "../../../../api/distribution-api.service";
-import {EMPTY} from "rxjs";
+import {DistributionApiService} from '../../../../api/distribution-api.service';
+import {FileHelperService} from '../../../../common/util/file-helper.service';
+import {HttpHeaders, HttpResponse} from '@angular/common/http';
+import {of} from 'rxjs';
 
 describe('RegisteredCustomersComponent', () => {
   let distributionApiService: jasmine.SpyObj<DistributionApiService>;
+  let fileHelperService: jasmine.SpyObj<FileHelperService>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -28,10 +31,15 @@ describe('RegisteredCustomersComponent', () => {
           provide: DistributionApiService,
           useValue: jasmine.createSpyObj('DistributionApiService', ['downloadCustomerList'])
         },
+        {
+          provide: FileHelperService,
+          useValue: jasmine.createSpyObj('FileHelperService', ['downloadFile'])
+        },
       ]
     }).compileComponents();
 
     distributionApiService = TestBed.inject(DistributionApiService) as jasmine.SpyObj<DistributionApiService>;
+    fileHelperService = TestBed.inject(FileHelperService) as jasmine.SpyObj<FileHelperService>;
   }));
 
   it('component can be created', () => {
@@ -55,11 +63,20 @@ describe('RegisteredCustomersComponent', () => {
     const fixture = TestBed.createComponent(RegisteredCustomersComponent);
     const component = fixture.componentInstance;
     fixture.detectChanges();
-    distributionApiService.downloadCustomerList.and.returnValue(EMPTY);
+
+    const response = new HttpResponse({
+      status: 200,
+      headers: new HttpHeaders(
+        {'Content-Disposition': 'inline; filename=test-1.pdf'}
+      ),
+      body: new Blob()
+    });
+    distributionApiService.downloadCustomerList.and.returnValue(of(response));
 
     component.downloadCustomerList();
 
     expect(distributionApiService.downloadCustomerList).toHaveBeenCalled();
+    expect(fileHelperService.downloadFile).toHaveBeenCalledWith('test-1.pdf', response.body);
   });
 
 });
