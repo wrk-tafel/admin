b255a7e3c9b407050123135e894ac08098f38c8a
Stephan Pantl <stephan.prantl@gmail.com>
2023-05-03 21:19:43 +0200

    moved toaster to root

--- frontend/src/main/webapp/src/app/app.component.ts
+++ frontend/src/main/webapp/src/app/app.component.ts
@@ -7,7 +7,7 @@ import {freeSet} from '@coreui/icons';
 @Component({
   // tslint:disable-next-line
   selector: 'body',
-  template: '<router-outlet></router-outlet>',
+  templateUrl: 'app.component.html',
   providers: [IconSetService],
 })
 export class AppComponent implements OnInit {
