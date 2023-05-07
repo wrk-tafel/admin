822776a23aac343e0c43bb89b7d23477ed76aa79
Stephan Pantl <stephan.prantl@gmail.com>
2023-05-07 16:52:07 +0200

    removed deprecated code

--- frontend/src/main/webapp/src/app/modules/checkin/ticket-screen-control/ticket-screen-control.component.ts
+++ frontend/src/main/webapp/src/app/modules/checkin/ticket-screen-control/ticket-screen-control.component.ts
@@ -1,11 +1,4 @@
-import {
-  Component,
-  ComponentFactory,
-  ComponentFactoryResolver,
-  ComponentRef,
-  ViewChild,
-  ViewContainerRef
-} from '@angular/core';
+import {Component, ComponentRef, ViewChild, ViewContainerRef} from '@angular/core';
 import {TicketScreenWindowComponent} from '../ticket-screen-window/ticket-screen-window.component';
 
 @Component({
@@ -17,13 +10,9 @@ export class TicketScreenControlComponent {
   @ViewChild('windowContainer', {read: ViewContainerRef}) windowContainer;
   componentRef: ComponentRef<TicketScreenWindowComponent>;
 
-  constructor(private resolver: ComponentFactoryResolver) {
-  }
-
   openScreenInNewTab() {
     this.windowContainer.clear();
-    const factory: ComponentFactory<TicketScreenWindowComponent> = this.resolver.resolveComponentFactory(TicketScreenWindowComponent);
-    this.componentRef = this.windowContainer.createComponent(factory);
+    this.componentRef = this.windowContainer.createComponent(TicketScreenWindowComponent);
   }
 
 }
