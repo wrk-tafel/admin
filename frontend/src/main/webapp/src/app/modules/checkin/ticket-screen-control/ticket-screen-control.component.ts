0ad64ed8baa01f96d8360f774e0c67fb857f282c
Stephan Pantl <stephan.prantl@gmail.com>
2023-05-11 23:25:39 +0200

    added more elements

--- frontend/src/main/webapp/src/app/modules/checkin/ticket-screen-control/ticket-screen-control.component.ts
+++ frontend/src/main/webapp/src/app/modules/checkin/ticket-screen-control/ticket-screen-control.component.ts
@@ -1,10 +1,15 @@
 import {Component} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'tafel-ticket-screen-control',
   templateUrl: 'ticket-screen-control.component.html'
 })
 export class TicketScreenControlComponent {
+
+  form = new FormGroup({
+    startDate: new FormControl<string>(null)
+  });
 
   openScreenInNewTab() {
     window.open('/#/anmeldung/ticketmonitor', '_blank');
