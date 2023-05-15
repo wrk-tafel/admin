178204a50e2b12fd14048ebc7e13fa0afa43a8e6
stephanprantl <stephan.prantl@gmail.com>
2023-05-15 22:37:57 +0200

    Improve ticket-screen layout (#538)
    * improved layout
    
    * improved
    
    * improved layout
    
    * improved layout
    
    * improved layout
    
    * improved errorhandling
    
    * improved e2e test

--- frontend/src/main/webapp/src/app/modules/checkin/ticket-screen-control/ticket-screen-control.component.ts
+++ frontend/src/main/webapp/src/app/modules/checkin/ticket-screen-control/ticket-screen-control.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {WebsocketService} from '../../../common/websocket/websocket.service';
 import {TicketScreenMessage} from '../ticket-screen/ticket-screen.component';
 import {DistributionApiService, TicketNumberResponse} from '../../../api/distribution-api.service';
@@ -17,7 +17,7 @@ export class TicketScreenControlComponent {
   }
 
   form = new FormGroup({
-    startTime: new FormControl<string>(null)
+    startTime: new FormControl<string>(null, Validators.required)
   });
 
   openScreenInNewTab() {
@@ -25,14 +25,18 @@ export class TicketScreenControlComponent {
   }
 
   showStartTime() {
-    const time = this.form.get('startTime').value;
-    const startTime = new Date();
-    startTime.setHours(Number(time.split(':')[0]));
-    startTime.setMinutes(Number(time.split(':')[1]));
-    startTime.setSeconds(0);
-    startTime.setMilliseconds(0);
+    this.form.markAllAsTouched();
 
-    this.sendToTicketScreen({startTime: startTime});
+    const time = this.startTime.value;
+    if (time) {
+      const startTime = new Date();
+      startTime.setHours(Number(time.split(':')[0]));
+      startTime.setMinutes(Number(time.split(':')[1]));
+      startTime.setSeconds(0);
+      startTime.setMilliseconds(0);
+
+      this.sendToTicketScreen({startTime: startTime});
+    }
   }
 
   showCurrentTicket() {
@@ -49,6 +53,10 @@ export class TicketScreenControlComponent {
 
   private sendToTicketScreen(message: TicketScreenMessage) {
     this.websocketService.publish({destination: '/topic/ticket-screen', body: JSON.stringify(message)});
+  }
+
+  get startTime() {
+    return this.form.get('startTime');
   }
 
 }
