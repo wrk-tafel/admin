cd17856ea185de36bc2d9fc7c3d220cc36319a4a
Stephan Pantl <stephan.prantl@gmail.com>
2023-05-16 23:28:45 +0200

    comment

---
+++ backend/src/main/kotlin/at/wrk/tafel/admin/backend/database/migration/MigrateDatabase.kt
@@ -0,0 +1,8 @@
+package at.wrk.tafel.admin.backend.database.migration
+
+/**
+ * Basic script to read the old database, migrate the data and write new sql's for the new database structure
+ */
+fun main() {
+
+}
