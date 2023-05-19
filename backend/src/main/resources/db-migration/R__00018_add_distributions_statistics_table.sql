3cc969406aae2b2ddc7fc73f4d553497d854af01
Stephan Pantl <stephan.prantl@gmail.com>
2023-05-19 02:32:52 +0200

    fixed syntax

--- backend/src/main/resources/db-migration/R__00018_add_distributions_statistics_table.sql
+++ backend/src/main/resources/db-migration/R__00018_add_distributions_statistics_table.sql
@@ -1,9 +1,9 @@
 create table if not exists distributions_statistics
 (
-    id                      bigint primary key,
-    created_at              timestamp   not null,
-    updated_at              timestamp   not null,
-    distribution_id         bigint      not null REFERENCES distributions (id) ON DELETE CASCADE,
+    id              bigint primary key,
+    created_at      timestamp not null,
+    updated_at      timestamp not null,
+    distribution_id bigint    not null REFERENCES distributions (id) ON DELETE CASCADE
     -- TODO
     -- lastname                varchar(50) not null,
     -- passwordchange_required boolean     not null default false
