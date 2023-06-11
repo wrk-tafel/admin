328c327c1b2995db855fb615e26c8260f6965871
Stephan Pantl <stephan.prantl@gmail.com>
2023-06-11 15:45:59 +0200

    fixed naming

--- backend/src/test/kotlin/at/wrk/tafel/admin/backend/AdminBackendApplicationIT.kt
+++ backend/src/test/kotlin/at/wrk/tafel/admin/backend/AdminBackendApplicationIT.kt
@@ -3,7 +3,7 @@ package at.wrk.tafel.admin.backend
 import at.wrk.tafel.admin.backend.common.TafelSpringBootIT
 import org.junit.jupiter.api.Test
 
-class AdminMigrationApplicationIT : TafelSpringBootIT() {
+class AdminBackendApplicationIT : TafelSpringBootIT() {
 
     @Test
     fun contextLoads() {
