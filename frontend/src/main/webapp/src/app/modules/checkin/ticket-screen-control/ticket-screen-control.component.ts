72bd527a7d189b20a4376dbf64fc31d16f4c6aee
stephanprantl <stephan.prantl@gmail.com>
2023-06-29 21:44:28 +0200

    Minor changes (#615)
    * fixed window open with reverse proxy
    
    * fixed import

--- frontend/src/main/webapp/src/app/modules/checkin/ticket-screen-control/ticket-screen-control.component.ts
+++ frontend/src/main/webapp/src/app/modules/checkin/ticket-screen-control/ticket-screen-control.component.ts
@@ -3,6 +3,7 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {WebsocketService} from '../../../common/websocket/websocket.service';
 import {TicketScreenMessage} from '../ticket-screen/ticket-screen.component';
 import {DistributionApiService, TicketNumberResponse} from '../../../api/distribution-api.service';
+import {UrlHelperService} from '../../../common/util/url-helper.service';
 
 @Component({
   selector: 'tafel-ticket-screen-control',
@@ -12,7 +13,8 @@ export class TicketScreenControlComponent {
 
   constructor(
     private websocketService: WebsocketService,
-    private distributionApiService: DistributionApiService
+    private distributionApiService: DistributionApiService,
+    private urlHelperService: UrlHelperService
   ) {
   }
 
@@ -21,7 +23,8 @@ export class TicketScreenControlComponent {
   });
 
   openScreenInNewTab() {
-    window.open('/#/anmeldung/ticketmonitor', '_blank');
+    const baseUrl = this.urlHelperService.getBaseUrl();
+    window.open(`${baseUrl}/#/anmeldung/ticketmonitor`, '_blank');
   }
 
   showStartTime() {
