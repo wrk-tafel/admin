e7d09f78d4fb22815c7ef00c8a6bb83f1a83cd9e
Stephan Pantl <stephan.prantl@gmail.com>
2023-06-29 21:20:14 +0200

    fixed window open with reverse proxy

--- frontend/src/main/webapp/src/app/modules/checkin/ticket-screen-control/ticket-screen-control.component.spec.ts
+++ frontend/src/main/webapp/src/app/modules/checkin/ticket-screen-control/ticket-screen-control.component.spec.ts
@@ -5,10 +5,12 @@ import {WebsocketService} from '../../../common/websocket/websocket.service';
 import {DistributionApiService, TicketNumberResponse} from '../../../api/distribution-api.service';
 import {TicketScreenMessage} from '../ticket-screen/ticket-screen.component';
 import {of} from 'rxjs';
+import {UrlHelperService} from '../../../common/util/url-helper.service';
 
 describe('TicketScreenControlComponent', () => {
   let websocketService: jasmine.SpyObj<WebsocketService>;
   let distributionApiService: jasmine.SpyObj<DistributionApiService>;
+  let urlHelperSpy: jasmine.SpyObj<UrlHelperService>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -21,12 +23,17 @@ describe('TicketScreenControlComponent', () => {
         {
           provide: DistributionApiService,
           useValue: jasmine.createSpyObj('DistributionApiService', ['getCurrentTicket', 'getNextTicket'])
+        },
+        {
+          provide: UrlHelperService,
+          useValue: jasmine.createSpyObj('UrlHelperService', ['getBaseUrl'])
         }
       ]
     }).compileComponents();
 
     websocketService = TestBed.inject(WebsocketService) as jasmine.SpyObj<WebsocketService>;
     distributionApiService = TestBed.inject(DistributionApiService) as jasmine.SpyObj<DistributionApiService>;
+    urlHelperSpy = TestBed.inject(UrlHelperService) as jasmine.SpyObj<UrlHelperService>;
   }));
 
   it('component can be created', () => {
@@ -38,11 +45,14 @@ describe('TicketScreenControlComponent', () => {
   it('openScreenInNewTab', () => {
     const fixture = TestBed.createComponent(TicketScreenControlComponent);
     const component = fixture.componentInstance;
+
+    const testBaseUrl = 'http://test:1234/testcontext';
+    urlHelperSpy.getBaseUrl.and.returnValue(testBaseUrl);
     spyOn(window, 'open');
 
     component.openScreenInNewTab();
 
-    expect(window.open).toHaveBeenCalledWith('/#/anmeldung/ticketmonitor', '_blank');
+    expect(window.open).toHaveBeenCalledWith(`${testBaseUrl}/#/anmeldung/ticketmonitor`, '_blank');
   });
 
   it('showStartTime', () => {
