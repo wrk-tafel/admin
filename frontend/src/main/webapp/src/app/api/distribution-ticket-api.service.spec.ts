35625f933c408f4bc7c9b508757d64797a1a1e47
Stephan Pantl <stephan.prantl@gmail.com>
2023-07-01 02:31:59 +0200

    cleanup

--- frontend/src/main/webapp/src/app/api/distribution-ticket-api.service.spec.ts
+++ frontend/src/main/webapp/src/app/api/distribution-ticket-api.service.spec.ts
@@ -1,27 +1,19 @@
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import {TestBed} from '@angular/core/testing';
 import {DistributionApiService} from './distribution-api.service';
-import {WebsocketService} from '../common/websocket/websocket.service';
 import {DistributionTicketApiService} from './distribution-ticket-api.service';
 
 describe('DistributionTicketApiService', () => {
   let httpMock: HttpTestingController;
-  let websocketService: jasmine.SpyObj<WebsocketService>;
   let apiService: DistributionTicketApiService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [DistributionApiService,
-        {
-          provide: WebsocketService,
-          useValue: jasmine.createSpyObj('WebsocketService', ['watch'])
-        }
-      ]
+      providers: [DistributionApiService]
     });
 
     httpMock = TestBed.inject(HttpTestingController);
-    websocketService = TestBed.inject(WebsocketService) as jasmine.SpyObj<WebsocketService>;
     apiService = TestBed.inject(DistributionTicketApiService);
   });
 
