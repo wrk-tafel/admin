12b13dfb8fafc88d2aa6a94a664b75a4c0f139e2
Stephan Pantl <stephan.prantl@gmail.com>
2023-07-04 00:15:57 +0200

    fixed syntax

--- backend/src/main/resources/db-migration/R__00021_cleanup_data.sql
+++ backend/src/main/resources/db-migration/R__00021_cleanup_data.sql
@@ -1,5 +1,5 @@
-update customers set income = null, incomeDue = null where income = 0;
-update customers_addpersons set income = null, incomeDue = null where income = 0;
+update customers set income = null, income_due = null where income = 0;
+update customers_addpersons set income = null, income_due = null where income = 0;
 
-update customers set incomeDue = null where incomeDue = '1900-01-01';
-update customers_addpersons set incomeDue = null where incomeDue = '1900-01-01';
+update customers set income_due = null where income_due = '1900-01-01';
+update customers_addpersons set income_due = null where income_due = '1900-01-01';
