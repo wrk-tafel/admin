708ef79f68f0ab78e05ed796d9690125a43eb44c
Stephan Pantl <stephan.prantl@gmail.com>
2023-07-04 00:09:35 +0200

    set also incomeDue to null

--- backend/src/main/resources/db-migration/R__00021_cleanup_data.sql
+++ backend/src/main/resources/db-migration/R__00021_cleanup_data.sql
@@ -1,2 +1,2 @@
-update customers set income = null where income = 0;
-update customers_addpersons set income = null where income = 0;
+update customers set income = null, incomeDue = null where income = 0;
+update customers_addpersons set income = null, incomeDue = null where income = 0;
