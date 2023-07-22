52918422b3a99f6b502e84ed2762c0155891d37b
Stephan Pantl <stephan.prantl@gmail.com>
2023-07-22 21:13:38 +0200

    formatted

--- backend/src/main/resources/db-migration/R__00022_add_familybonus_field.sql
+++ backend/src/main/resources/db-migration/R__00022_add_familybonus_field.sql
@@ -4,7 +4,5 @@ alter table if exists customers_addpersons
     add column if not exists receives_familybonus bool default false not null;
 
 -- update existing data as we assumed it currently that it's given
-update customers
-set receives_familybonus = true;
-update customers_addpersons
-set receives_familybonus = true;
+update customers set receives_familybonus = true;
+update customers_addpersons set receives_familybonus = true;
