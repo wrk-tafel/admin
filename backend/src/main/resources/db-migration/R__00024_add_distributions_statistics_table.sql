ed28eab3730074acafab686ac24154a825cb682b
Stephan Pantl <stephan.prantl@gmail.com>
2023-07-25 20:34:44 +0200

    enhanced table

--- backend/src/main/resources/db-migration/R__00024_add_distributions_statistics_table.sql
+++ backend/src/main/resources/db-migration/R__00024_add_distributions_statistics_table.sql
@@ -1,10 +1,13 @@
 create table if not exists distributions_statistics
 (
-    id              bigint primary key,
-    created_at      timestamp not null,
-    updated_at      timestamp not null,
-    distribution_id bigint    not null REFERENCES distributions (id) ON DELETE CASCADE
-    -- TODO
-    -- lastname                varchar(50) not null,
-    -- passwordchange_required boolean     not null default false
+    id                           bigint primary key,
+    created_at                   timestamp not null,
+    updated_at                   timestamp not null,
+    distribution_id              bigint    not null REFERENCES distributions (id) ON DELETE CASCADE,
+    count_customers              integer   not null,
+    count_persons                integer   not null,
+    count_infants                integer   not null,
+    average_persons_per_customer decimal   not null,
+    count_customers_new          integer   not null,
+    count_customers_updated      integer   not null
 );
