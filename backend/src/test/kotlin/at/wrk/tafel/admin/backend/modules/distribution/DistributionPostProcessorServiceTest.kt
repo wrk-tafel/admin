958603f0db051b8d482463e6bc0f69bbed4ecc7e
Stephan Pantl <stephan.prantl@gmail.com>
2023-08-04 18:21:24 +0200

    enhanced test

--- backend/src/test/kotlin/at/wrk/tafel/admin/backend/modules/distribution/DistributionPostProcessorServiceTest.kt
+++ backend/src/test/kotlin/at/wrk/tafel/admin/backend/modules/distribution/DistributionPostProcessorServiceTest.kt
@@ -1,13 +1,15 @@
 package at.wrk.tafel.admin.backend.modules.distribution
 
 import at.wrk.tafel.admin.backend.database.entities.distribution.DistributionEntity
+import at.wrk.tafel.admin.backend.database.entities.distribution.DistributionStatisticEntity
 import at.wrk.tafel.admin.backend.modules.distribution.statistic.DistributionStatisticService
+import at.wrk.tafel.admin.backend.modules.reporting.DailyReportService
+import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.junit5.MockKExtension
 import io.mockk.mockk
 import io.mockk.verify
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 
@@ -17,16 +19,22 @@ internal class DistributionPostProcessorServiceTest {
     @RelaxedMockK
     private lateinit var distributionStatisticService: DistributionStatisticService
 
+    @RelaxedMockK
+    private lateinit var dailyReportService: DailyReportService
+
     @InjectMockKs
     private lateinit var service: DistributionPostProcessorService
 
     @Test
     fun `process calls proper services`() {
         val distribution = mockk<DistributionEntity>()
+        val distributionStatistic = mockk<DistributionStatisticEntity>()
+        every { distributionStatisticService.createAndSaveStatistic(distribution) } returns distributionStatistic
 
         service.process(distribution)
 
         verify { distributionStatisticService.createAndSaveStatistic(distribution) }
+        verify { dailyReportService.generateDailyReportPdf(distributionStatistic) }
     }
 
 }
