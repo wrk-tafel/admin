09597cd2c13827c982028ea112ccbd517eed2ff0
Stephan Pantl <stephan.prantl@gmail.com>
2023-08-29 21:08:10 +0200

    added test

---
+++ backend/src/test/kotlin/at/wrk/tafel/admin/backend/architecture/ModularityTests.java
@@ -0,0 +1,22 @@
+package at.wrk.tafel.admin.backend.architecture;
+
+import at.wrk.tafel.admin.backend.AdminBackendApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
+
+class ModularityTests {
+
+    ApplicationModules modules = ApplicationModules.of(AdminBackendApplication.class);
+
+    @Test
+    void verifiesModularStructure() {
+        modules.verify();
+    }
+
+    @Test
+    void createModuleDocumentation() {
+        new Documenter(modules).writeDocumentation();
+    }
+
+}
