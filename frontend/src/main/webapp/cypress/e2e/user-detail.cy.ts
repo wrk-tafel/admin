c68ff39965e55676e07ef8675e021b81d5a61d96
Stephan Pantl <stephan.prantl@gmail.com>
2023-09-10 18:09:27 +0200

    added test

--- frontend/src/main/webapp/cypress/e2e/user-detail.cy.ts
+++ frontend/src/main/webapp/cypress/e2e/user-detail.cy.ts
@@ -9,4 +9,20 @@ describe('User Detail', () => {
     cy.byTestId('usernameText').should('have.text', 'admin');
   });
 
+  it('disable and re-enable user', () => {
+    cy.visit('/#/benutzer/detail/300');
+
+    cy.byTestId('enabledText').should('have.text', 'Ja');
+
+    cy.byTestId('changeUserStateButton').click();
+    cy.byTestId('disableUserButton').click();
+
+    cy.byTestId('enabledText').should('have.text', 'Nein');
+
+    cy.byTestId('changeUserStateButton').click();
+    cy.byTestId('enableUserButton').click();
+
+    cy.byTestId('enabledText').should('have.text', 'Ja');
+  });
+
 });
