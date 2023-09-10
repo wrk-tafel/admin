a0d9cf2089bd30ae8b73333be109068171f4d7d4
Stephan Pantl <stephan.prantl@gmail.com>
2023-09-10 12:48:26 +0200

    add basic data

--- frontend/src/main/webapp/src/app/modules/user/user-detail/user-detail.component.ts
+++ frontend/src/main/webapp/src/app/modules/user/user-detail/user-detail.component.ts
@@ -1,8 +1,21 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserData} from "../../../api/user-api.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'tafel-user-detail',
   templateUrl: 'user-detail.component.html'
 })
-export class UserDetailComponent {
+export class UserDetailComponent implements OnInit {
+  userData: UserData;
+
+  constructor(
+    private activatedRoute: ActivatedRoute
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.userData = this.activatedRoute.snapshot.data.userData;
+  }
+
 }
--- frontend/src/main/webapp/src/app/modules/user/user.module.ts
+++ frontend/src/main/webapp/src/app/modules/user/user.module.ts
@@ -2,14 +2,27 @@ import {NgModule} from '@angular/core';
 import {UserPasswordChangeComponent} from './user-passwordchange/user-passwordchange.component';
 import {UserRoutingModule} from './user-routing.module';
 import {
-  ButtonDirective,
-  CardBodyComponent,
-  CardComponent,
-  CardFooterComponent,
-  CardHeaderComponent,
-  ColComponent,
-  RowComponent,
-  TableDirective
+    BadgeComponent,
+    BgColorDirective,
+    ButtonCloseDirective,
+    ButtonDirective,
+    CardBodyComponent,
+    CardComponent,
+    CardFooterComponent,
+    CardHeaderComponent,
+    ColComponent,
+    DropdownComponent,
+    DropdownDividerDirective,
+    DropdownItemDirective,
+    DropdownMenuDirective,
+    DropdownToggleDirective, FormSelectDirective,
+    ModalBodyComponent,
+    ModalComponent,
+    ModalFooterComponent,
+    ModalHeaderComponent,
+    NavComponent, NavItemComponent, NavLinkDirective, RoundedDirective,
+    RowComponent, TabContentComponent,
+    TableDirective, TabPaneComponent
 } from '@coreui/angular';
 import {TafelCommonModule} from '../../common/tafel-common.module';
 import {CommonModule} from '@angular/common';
@@ -18,20 +31,39 @@ import {ReactiveFormsModule} from '@angular/forms';
 import {UserDetailComponent} from "./user-detail/user-detail.component";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    TafelCommonModule,
-    UserRoutingModule,
-    CardFooterComponent,
-    CardHeaderComponent,
-    CardComponent,
-    CardBodyComponent,
-    ButtonDirective,
-    ColComponent,
-    ReactiveFormsModule,
-    RowComponent,
-    TableDirective
-  ],
+    imports: [
+        CommonModule,
+        TafelCommonModule,
+        UserRoutingModule,
+        CardFooterComponent,
+        CardHeaderComponent,
+        CardComponent,
+        CardBodyComponent,
+        ButtonDirective,
+        ColComponent,
+        ReactiveFormsModule,
+        RowComponent,
+        TableDirective,
+        BgColorDirective,
+        ButtonCloseDirective,
+        DropdownComponent,
+        DropdownDividerDirective,
+        DropdownItemDirective,
+        DropdownMenuDirective,
+        DropdownToggleDirective,
+        ModalBodyComponent,
+        ModalComponent,
+        ModalFooterComponent,
+        ModalHeaderComponent,
+        NavComponent,
+        NavItemComponent,
+        NavLinkDirective,
+        RoundedDirective,
+        TabContentComponent,
+        TabPaneComponent,
+        BadgeComponent,
+        FormSelectDirective
+    ],
   declarations: [
     UserPasswordChangeComponent,
     UserDetailComponent,
