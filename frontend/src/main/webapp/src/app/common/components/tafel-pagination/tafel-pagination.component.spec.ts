87330f6b03eea72884c66e7519bdbecf94399f3a
Stephan Pantl <stephan.prantl@gmail.com>
2023-10-08 05:19:53 +0200

    added tests

--- frontend/src/main/webapp/src/app/common/components/tafel-pagination/tafel-pagination.component.spec.ts
+++ frontend/src/main/webapp/src/app/common/components/tafel-pagination/tafel-pagination.component.spec.ts
@@ -1,10 +1,14 @@
 import {TestBed, waitForAsync} from '@angular/core/testing';
 import {TafelPaginationComponent} from './tafel-pagination.component';
+import {PaginationComponent, PaginationModule} from '@coreui/angular';
 
 describe('TafelPaginationComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [],
+      imports: [
+        PaginationModule,
+        PaginationComponent
+      ],
       providers: [],
       declarations: [TafelPaginationComponent]
     }).compileComponents();
@@ -15,6 +19,70 @@ describe('TafelPaginationComponent', () => {
     const component = fixture.componentInstance;
 
     expect(component).toBeTruthy();
+  }));
+
+  it('pagination for single page', waitForAsync(() => {
+    const fixture = TestBed.createComponent(TafelPaginationComponent);
+    const component = fixture.componentInstance;
+    component.paginationData = {
+      count: 5,
+      totalCount: 5,
+      currentPage: 0,
+      totalPages: 1,
+      pageSize: 10
+    };
+
+    expect(component.currentPage).toBe(1);
+    expect(component.maxPage).toBe(1);
+    expect(component.text).toBe('1 - 5 von 5');
+  }));
+
+  it('pagination for multiple pages, selected first page', waitForAsync(() => {
+    const fixture = TestBed.createComponent(TafelPaginationComponent);
+    const component = fixture.componentInstance;
+    component.paginationData = {
+      count: 5,
+      totalCount: 50,
+      currentPage: 0,
+      totalPages: 10,
+      pageSize: 10
+    };
+
+    expect(component.currentPage).toBe(1);
+    expect(component.maxPage).toBe(10);
+    expect(component.text).toBe('1 - 5 von 50');
+  }));
+
+  it('pagination for multiple pages, selected second page', waitForAsync(() => {
+    const fixture = TestBed.createComponent(TafelPaginationComponent);
+    const component = fixture.componentInstance;
+    component.paginationData = {
+      count: 10,
+      totalCount: 30,
+      currentPage: 1,
+      totalPages: 2,
+      pageSize: 10
+    };
+
+    expect(component.currentPage).toBe(2);
+    expect(component.maxPage).toBe(2);
+    expect(component.text).toBe('11 - 20 von 30');
+  }));
+
+  it('pagination for multiple pages, selected last page', waitForAsync(() => {
+    const fixture = TestBed.createComponent(TafelPaginationComponent);
+    const component = fixture.componentInstance;
+    component.paginationData = {
+      count: 8,
+      totalCount: 28,
+      currentPage: 2,
+      totalPages: 3,
+      pageSize: 10
+    };
+
+    expect(component.currentPage).toBe(3);
+    expect(component.maxPage).toBe(3);
+    expect(component.text).toBe('21 - 28 von 28');
   }));
 
 });
