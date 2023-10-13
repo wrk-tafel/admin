bc66728c8a7b643852c6b4adbd33f6d8ab888058
Stephan Pantl <stephan.prantl@gmail.com>
2023-10-13 19:33:17 +0200

    added api client

--- frontend/src/main/webapp/src/app/api/customer-note-api.service.ts
+++ frontend/src/main/webapp/src/app/api/customer-note-api.service.ts
@@ -1,4 +1,4 @@
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 
@@ -11,8 +11,12 @@ export class CustomerNoteApiService {
   ) {
   }
 
-  getNotesForCustomer(customerId: number): Observable<CustomerNotesResponse> {
-    return this.http.get<CustomerNotesResponse>(`/customers/${customerId}/notes`);
+  getNotesForCustomer(customerId: number, page?: number): Observable<CustomerNotesResponse> {
+    let queryParams = new HttpParams();
+    if (page) {
+      queryParams = queryParams.set('page', page);
+    }
+    return this.http.get<CustomerNotesResponse>(`/customers/${customerId}/notes`, {params: queryParams});
   }
 
   createNewNote(customerId: number, note: string): Observable<CustomerNoteItem> {
