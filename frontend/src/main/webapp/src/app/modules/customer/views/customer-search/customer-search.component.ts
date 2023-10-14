c28e833350d3b6fc33c0d3c0027ba6436c21b95d
Stephan Pantl <stephan.prantl@gmail.com>
2023-10-14 23:06:15 +0200

    improved format

--- frontend/src/main/webapp/src/app/modules/customer/views/customer-search/customer-search.component.ts
+++ frontend/src/main/webapp/src/app/modules/customer/views/customer-search/customer-search.component.ts
@@ -6,91 +6,90 @@ import {ToastService, ToastType} from '../../../../common/views/default-layout/t
 import {TafelPaginationData} from '../../../../common/components/tafel-pagination/tafel-pagination.component';
 
 @Component({
-  selector: 'tafel-customer-search',
-  templateUrl: 'customer-search.component.html'
+    selector: 'tafel-customer-search',
+    templateUrl: 'customer-search.component.html'
 })
 export class CustomerSearchComponent {
-  searchResult: CustomerSearchResult;
-  customerSearchForm = new FormGroup({
-    customerId: new FormControl<number>(null),
-    lastname: new FormControl<string>(null),
-    firstname: new FormControl<string>(null),
-    postProcessing: new FormControl<boolean>(null),
-  });
-  paginationData: TafelPaginationData;
+    searchResult: CustomerSearchResult;
+    customerSearchForm = new FormGroup({
+        customerId: new FormControl<number>(null),
+        lastname: new FormControl<string>(null),
+        firstname: new FormControl<string>(null),
+        postProcessing: new FormControl<boolean>(null),
+    });
+    paginationData: TafelPaginationData;
 
-  constructor(
-    private customerApiService: CustomerApiService,
-    private router: Router,
-    private toastService: ToastService
-  ) {
-  }
-
-  get customerId() {
-    return this.customerSearchForm.get('customerId');
-  }
-
-  get lastname() {
-    return this.customerSearchForm.get('lastname');
-  }
-
-  get firstname() {
-    return this.customerSearchForm.get('firstname');
-  }
-
-  get postProcessing() {
-    return this.customerSearchForm.get('postProcessing');
-  }
-
-  searchForCustomerId() {
-    const customerId = this.customerId.value;
-
-    /* eslint-disable @typescript-eslint/no-unused-vars */
-    const observer = {
-      next: (response) => this.router.navigate(['/kunden/detail', customerId])
-    };
-    this.customerApiService.getCustomer(customerId).subscribe(observer);
-  }
-
-  searchForDetails(page?: number) {
-    this.customerApiService.searchCustomer(this.lastname.value, this.firstname.value, this.postProcessing.value, page)
-      .subscribe((response: CustomerSearchResult) => {
-        if (response.items.length === 0) {
-          this.toastService.showToast({type: ToastType.INFO, title: 'Keine Kunden gefunden!'});
-          this.searchResult = null;
-          this.paginationData = null;
-        } else {
-          this.searchResult = response;
-          this.paginationData = {
-            count: response.items.length,
-            totalCount: response.totalCount,
-            currentPage: response.currentPage,
-            totalPages: response.totalPages,
-            pageSize: response.pageSize
-          };
-        }
-      });
-  }
-
-  navigateToCustomer(customerId: number) {
-    this.router.navigate(['/kunden/detail', customerId]);
-  }
-
-  editCustomer(customerId: number) {
-    this.router.navigate(['/kunden/bearbeiten', customerId]);
-  }
-
-  formatAddress(address: CustomerAddressData): string {
-    let result = '';
-    result += address.street + ' ' + address.houseNumber;
-    if (address.stairway) {
-      result += ', Stiege ' + address.stairway;
+    constructor(
+        private customerApiService: CustomerApiService,
+        private router: Router,
+        private toastService: ToastService
+    ) {
     }
-    if (address.stairway) {
-      result += ', Top ' + address.door;
+
+    get customerId() {
+        return this.customerSearchForm.get('customerId');
     }
-    result += ' / ' + address.postalCode + ' ' + address.city;
-    return result;
-  }
+
+    get lastname() {
+        return this.customerSearchForm.get('lastname');
+    }
+
+    get firstname() {
+        return this.customerSearchForm.get('firstname');
+    }
+
+    get postProcessing() {
+        return this.customerSearchForm.get('postProcessing');
+    }
+
+    searchForCustomerId() {
+        const customerId = this.customerId.value;
+
+        /* eslint-disable @typescript-eslint/no-unused-vars */
+        const observer = {
+            next: (response) => this.router.navigate(['/kunden/detail', customerId])
+        };
+        this.customerApiService.getCustomer(customerId).subscribe(observer);
+    }
+
+    searchForDetails(page?: number) {
+        this.customerApiService.searchCustomer(this.lastname.value, this.firstname.value, this.postProcessing.value, page)
+            .subscribe((response: CustomerSearchResult) => {
+                if (response.items.length === 0) {
+                    this.toastService.showToast({type: ToastType.INFO, title: 'Keine Kunden gefunden!'});
+                    this.searchResult = null;
+                    this.paginationData = null;
+                } else {
+                    this.searchResult = response;
+                    this.paginationData = {
+                        count: response.items.length,
+                        totalCount: response.totalCount,
+                        currentPage: response.currentPage,
+                        totalPages: response.totalPages,
+                        pageSize: response.pageSize
+                    };
+                }
+            });
+    }
+
+    navigateToCustomer(customerId: number) {
+        this.router.navigate(['/kunden/detail', customerId]);
+    }
+
+    editCustomer(customerId: number) {
+        this.router.navigate(['/kunden/bearbeiten', customerId]);
+    }
+
+    formatAddress(address: CustomerAddressData): string {
+        const formatted = [
+            [address.street, address.houseNumber].join(' '),
+            address.stairway ? 'Stiege ' + address.stairway : undefined,
+            address.door ? 'Top ' + address.door : undefined,
+            [address.postalCode, address.city].join(' ')
+        ]
+            .filter(value => value?.trim().length > 0)
+            .join(', ');
+        return formatted?.trim().length > 0 ? formatted : '-';
+    }
 
 }
