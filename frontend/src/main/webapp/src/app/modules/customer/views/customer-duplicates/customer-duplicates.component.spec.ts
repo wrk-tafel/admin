e6195e5b5a6b0188e69700b19c142bae468e2104
Stephan Pantl <stephan.prantl@gmail.com>
2023-10-23 21:54:53 +0200

    added test

--- frontend/src/main/webapp/src/app/modules/customer/views/customer-duplicates/customer-duplicates.component.spec.ts
+++ frontend/src/main/webapp/src/app/modules/customer/views/customer-duplicates/customer-duplicates.component.spec.ts
@@ -1,9 +1,58 @@
 import {TestBed, waitForAsync} from '@angular/core/testing';
-import {CustomerApiService} from '../../../../api/customer-api.service';
+import {CustomerApiService, CustomerDuplicatesResponse, Gender} from '../../../../api/customer-api.service';
 import {CustomerDuplicatesComponent} from './customer-duplicates.component';
+import {ActivatedRoute} from '@angular/router';
+import * as moment from "moment/moment";
 
 describe('CustomerDuplicatesComponent', () => {
   let customerApiService: jasmine.SpyObj<CustomerApiService>;
+
+  const mockCustomer1 = {
+    id: 133,
+    lastname: 'Mustermann',
+    firstname: 'Max',
+    birthDate: moment().subtract(30, 'years').startOf('day').utc().toDate(),
+    gender: Gender.MALE,
+    address: {
+      street: 'Teststraße',
+      houseNumber: '123A',
+      door: '21',
+      postalCode: 1020,
+      city: 'Wien',
+    },
+    employer: 'test employer',
+    income: 1000
+  };
+
+  const mockCustomer2 = {
+    id: 233,
+    lastname: 'Mustermann',
+    firstname: 'Max',
+    birthDate: moment().subtract(30, 'years').startOf('day').utc().toDate(),
+    gender: Gender.MALE,
+    address: {
+      street: 'Teststraße',
+      houseNumber: '123A',
+      door: '21',
+      postalCode: 1020,
+      city: 'Wien',
+    },
+    employer: 'test employer',
+    income: 1000
+  };
+
+  const mockCustomerDuplicatesDataResponse: CustomerDuplicatesResponse = {
+    items: [
+      {
+        customer: mockCustomer1,
+        similarCustomers: [mockCustomer2]
+      }
+    ],
+    totalCount: 100,
+    currentPage: 3,
+    totalPages: 10,
+    pageSize: 10
+  };
 
   beforeEach(waitForAsync(() => {
     const customerApiServiceSpy = jasmine.createSpyObj('CustomerApiService', ['generatePdf', 'deleteCustomer', 'updateCustomer']);
@@ -17,7 +66,17 @@ describe('CustomerDuplicatesComponent', () => {
         {
           provide: CustomerApiService,
           useValue: customerApiServiceSpy
-        }
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              data: {
+                customerDuplicatesData: mockCustomerDuplicatesDataResponse
+              }
+            }
+          }
+        },
       ]
     }).compileComponents();
 
@@ -29,6 +88,22 @@ describe('CustomerDuplicatesComponent', () => {
     const component = fixture.componentInstance;
 
     expect(component).toBeTruthy();
+  });
+
+  it('init fills data correctly', () => {
+    const fixture = TestBed.createComponent(CustomerDuplicatesComponent);
+    const component = fixture.componentInstance;
+
+    component.ngOnInit();
+
+    expect(component.customerDuplicatesData).toEqual(mockCustomerDuplicatesDataResponse);
+    expect(component.paginationData).toEqual({
+      count: mockCustomerDuplicatesDataResponse.items.length,
+      totalCount: mockCustomerDuplicatesDataResponse.totalCount,
+      currentPage: mockCustomerDuplicatesDataResponse.currentPage,
+      totalPages: mockCustomerDuplicatesDataResponse.totalPages,
+      pageSize: mockCustomerDuplicatesDataResponse.pageSize
+    });
   });
 
 });
