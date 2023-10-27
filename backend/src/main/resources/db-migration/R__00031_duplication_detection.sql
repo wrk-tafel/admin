e7f21ad937a072b4c1e8b961ade0b8c0f728c484
Stephan Pantl <stephan.prantl@gmail.com>
2023-10-27 22:23:15 +0200

    disabled migration file

--- backend/src/main/resources/db-migration/R__00031_duplication_detection.sql
+++ backend/src/main/resources/db-migration/R__00031_duplication_detection.sql
@@ -1,4 +1,5 @@
-create extension if not exists fuzzystrmatch;
+-- Executed manually due to missing permissions
 
-create index if not exists soundex_firstname ON customers (soundex(firstname));
-create index if not exists soundex_lastname ON customers (soundex(lastname));
+-- create extension if not exists fuzzystrmatch;
+-- create index if not exists soundex_firstname ON customers (soundex(firstname));
+-- create index if not exists soundex_lastname ON customers (soundex(lastname));
