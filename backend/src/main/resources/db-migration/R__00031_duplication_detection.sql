e3be26450f7851f930cd1b2658ba3db0e51dae84
Stephan Pantl <stephan.prantl@gmail.com>
2023-10-27 22:25:28 +0200

    keep index for testdata db

--- backend/src/main/resources/db-migration/R__00031_duplication_detection.sql
+++ backend/src/main/resources/db-migration/R__00031_duplication_detection.sql
@@ -1,5 +1,5 @@
 -- Executed manually due to missing permissions
-
 -- create extension if not exists fuzzystrmatch;
--- create index if not exists soundex_firstname ON customers (soundex(firstname));
--- create index if not exists soundex_lastname ON customers (soundex(lastname));
+
+create index if not exists soundex_firstname ON customers (soundex(firstname));
+create index if not exists soundex_lastname ON customers (soundex(lastname));
