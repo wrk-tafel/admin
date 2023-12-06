12ff5946a83ed940e879dd73c09fbef97832ed02
stephanprantl <stephan.prantl@gmail.com>
2023-12-06 23:43:48 +0100

    Update template (#953)
    * updated template to angular 17
    
    * removed ignores

--- frontend/src/main/template/src/app/containers/default-layout/default-layout.component.ts
+++ frontend/src/main/template/src/app/containers/default-layout/default-layout.component.ts
@@ -5,14 +5,11 @@ import { navItems } from './_nav';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './default-layout.component.html',
+  styleUrls: ['./default-layout.component.scss'],
 })
 export class DefaultLayoutComponent {
 
   public navItems = navItems;
-
-  public perfectScrollbarConfig = {
-    suppressScrollX: true,
-  };
 
   constructor() {}
 }
