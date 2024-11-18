45ec975e9bce9e6223683464092f397eca0e212d
stephanprantl <stephan.prantl@gmail.com>
2024-11-19 00:02:18 +0100

    migrated customers_notes to employee instead user_id (#1568)

--- backend/src/main/kotlin/at/wrk/tafel/admin/backend/database/entities/customer/CustomerNoteEntity.kt
+++ backend/src/main/kotlin/at/wrk/tafel/admin/backend/database/entities/customer/CustomerNoteEntity.kt
@@ -1,8 +1,8 @@
 package at.wrk.tafel.admin.backend.database.entities.customer
 
 import at.wrk.tafel.admin.backend.common.ExcludeFromTestCoverage
-import at.wrk.tafel.admin.backend.database.entities.auth.UserEntity
 import at.wrk.tafel.admin.backend.database.entities.base.BaseChangeTrackingEntity
+import at.wrk.tafel.admin.backend.database.entities.base.EmployeeEntity
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.JoinColumn
@@ -19,8 +19,8 @@ class CustomerNoteEntity : BaseChangeTrackingEntity() {
     var customer: CustomerEntity? = null
 
     @ManyToOne
-    @JoinColumn(name = "user_id", nullable = true)
-    var user: UserEntity? = null
+    @JoinColumn(name = "employee_id", nullable = true)
+    var employee: EmployeeEntity? = null
 
     @Column(name = "note")
     var note: String? = null
