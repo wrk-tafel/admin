19570d93fe15c9bb6ef5ecd03e48db84ec14e39e
Stephan Pantl <stephan.prantl@gmail.com>
2025-03-07 19:09:12 +0100

    uncommented file

--- backend/src/main/resources/db-migration/R__00004_customer_tables.sql
+++ backend/src/main/resources/db-migration/R__00004_customer_tables.sql
@@ -1,3 +1,4 @@
+/*
 DROP TABLE IF EXISTS customers_addpersons;
 DROP TABLE IF EXISTS customers;
 
@@ -40,3 +41,4 @@ create table customers_addpersons
     income      decimal     null,
     income_due  date        null
 );
+*/
