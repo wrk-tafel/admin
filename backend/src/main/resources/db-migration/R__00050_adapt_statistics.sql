87577c34893563a2f4cb91db6041cdae0e09a02c
Stephan Pantl <stephan.prantl@gmail.com>
2025-03-07 19:37:40 +0100

    adapted sql migration

--- backend/src/main/resources/db-migration/R__00050_adapt_statistics.sql
+++ backend/src/main/resources/db-migration/R__00050_adapt_statistics.sql
@@ -26,31 +26,31 @@ alter table distributions_statistics
     alter column count_persons_prolonged set default 0;
 
 alter table distributions_statistics
-    alter column shops_total_count set not null;
-
-alter table distributions_statistics
     alter column shops_total_count set default 0;
 
 alter table distributions_statistics
-    alter column shops_with_food_count set not null;
+    alter column shops_total_count set not null;
 
 alter table distributions_statistics
     alter column shops_with_food_count set default 0;
 
 alter table distributions_statistics
-    alter column food_total_amount set not null;
+    alter column shops_with_food_count set not null;
 
 alter table distributions_statistics
     alter column food_total_amount set default 0;
 
 alter table distributions_statistics
-    alter column food_per_shop_average set not null;
+    alter column food_total_amount set not null;
 
 alter table distributions_statistics
     alter column food_per_shop_average set default 0;
 
 alter table distributions_statistics
-    alter column routes_length_km set not null;
+    alter column food_per_shop_average set not null;
 
 alter table distributions_statistics
     alter column routes_length_km set default 0;
+
+alter table distributions_statistics
+    alter column routes_length_km set not null;
