a79fc0a678ead5777684b1850ceb6063e72e7ad6
Stephan Pantl <stephan.prantl@gmail.com>
2025-03-07 19:52:45 +0100

    improved migration

--- backend/src/main/resources/db-migration/R__00050_adapt_statistics.sql
+++ backend/src/main/resources/db-migration/R__00050_adapt_statistics.sql
@@ -28,11 +28,19 @@ alter table distributions_statistics
 alter table distributions_statistics
     alter column shops_total_count set default 0;
 
+update distributions_statistics
+set shops_total_count = 0
+where shops_total_count is null;
+
 alter table distributions_statistics
     alter column shops_total_count set not null;
 
 alter table distributions_statistics
     alter column shops_with_food_count set default 0;
+
+update distributions_statistics
+set shops_with_food_count = 0
+where shops_with_food_count is null;
 
 alter table distributions_statistics
     alter column shops_with_food_count set not null;
@@ -40,17 +48,29 @@ alter table distributions_statistics
 alter table distributions_statistics
     alter column food_total_amount set default 0;
 
+update distributions_statistics
+set food_total_amount = 0
+where food_total_amount is null;
+
 alter table distributions_statistics
     alter column food_total_amount set not null;
 
 alter table distributions_statistics
     alter column food_per_shop_average set default 0;
 
+update distributions_statistics
+set food_per_shop_average = 0
+where food_per_shop_average is null;
+
 alter table distributions_statistics
     alter column food_per_shop_average set not null;
 
 alter table distributions_statistics
     alter column routes_length_km set default 0;
+
+update distributions_statistics
+set routes_length_km = 0
+where routes_length_km is null;
 
 alter table distributions_statistics
     alter column routes_length_km set not null;
