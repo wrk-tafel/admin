d425c9fd63c04fd6234ad94074acfc7cdc52843b
Stephan Pantl <stephan.prantl@gmail.com>
2025-05-19 23:33:16 +0200

    added transactional annotation

--- backend/src/main/kotlin/at/wrk/tafel/admin/backend/database/common/sse_outbox/SseOutboxRepository.kt
+++ backend/src/main/kotlin/at/wrk/tafel/admin/backend/database/common/sse_outbox/SseOutboxRepository.kt
@@ -1,10 +1,12 @@
 package at.wrk.tafel.admin.backend.database.common.sse_outbox
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDateTime
 
 interface SseOutboxRepository : JpaRepository<SseOutboxEntity, Long> {
 
+    @Transactional
     fun deleteAllByEventTimeBefore(eventTime: LocalDateTime)
 
 }
